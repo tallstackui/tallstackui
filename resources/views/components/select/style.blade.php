0654a9b12999f1234ba0042a9f009f409e30c833
AJ Meireles <alvaro.meireles@live.com>
2023-09-16 22:27:36 -0300

    wip

--- resources/views/components/select/style.blade.php
+++ resources/views/components/select/style.blade.php
@@ -6,7 +6,7 @@
 <div x-cloak x-data="{
         show : false,
         model : @entangle($computed),
-        selecteds : null,
+        selecteds : [],
         search: '',
         searchable : @js($searchable),
         dimensional : @js($selectable !== []),
@@ -23,24 +23,44 @@
                     ? this.selecteds[this.selectable.label]
                     : this.selecteds;
             }
+
+            if (this.multiple && !this.selecteds) {
+                this.selecteds = [];
+            }
         },
         select (option) {
-            if (this.selecteds && this.selected(option)) {
+            if (!this.multiple && this.selecteds && this.selected(option)) {
                 this.clear();
                 return;
             }
 
-            this.selecteds = option;
+            if (this.multiple && this.selected(option)) {
+                this.clear(option);
+                return;
+            }
+
+            if (this.multiple && !this.selecteds) {
+                this.selecteds = [];
+            }
+
+            this.selecteds = this.multiple
+                ? [...this.selecteds, option]
+                : [option];
 
             if (this.dimensional) {
-                this.model = option[this.selectable.value];
+                console.log(this.selecteds);
+                this.model = this.multiple
+                    ? this.selecteds.map(selected => selected[this.selectable.value])
+                    : option[this.selectable.value];
                 this.placeholder = option[this.selectable.label];
             } else {
-                this.model = option;
+                this.model = this.multiple
+                    ? this.selecteds
+                    : option;
                 this.placeholder = option;
             }
 
-            this.show = false;
+            this.show = this.multiple;
             this.search = '';
         },
         selected (option) {
@@ -50,21 +70,55 @@
                 return this.selecteds === option;
             }
 
-            const keys   = Object.keys(this.selecteds);
-            const values = Object.values(this.selecteds);
+            const selecteds = this.multiple && this.selecteds.length === 1
+                ? this.selecteds[0]
+                : this.selecteds;
+
+            if (this.multiple && selecteds.length >= 1) {
+                return selecteds.some(selected => {
+                    const keys   = Object.keys(selected);
+                    const values = Object.values(selected);
+
+                    return keys.every(key => {
+                        return selected[key] === option[key];
+                    }) && values.every(value => {
+                        return selected[value] === option[value];
+                    });
+                });
+            }
+
+            const keys   = Object.keys(selecteds);
+            const values = Object.values(selecteds);
+
+            if (keys.length === 0 && values.length === 0) {
+                return false;
+            }
 
             return keys.every(key => {
-                return this.selecteds[key] === option[key];
+                return selecteds[key] === option[key];
             }) && values.every(value => {
-                return this.selecteds[value] === option[value];
+                return selecteds[value] === option[value];
             });
         },
-        clear() {
+        clear(selected = null) {
+            if (this.multiple && selected) {
+                this.selecteds = this.selecteds.filter(option => option !== selected);
+
+                if (this.selecteds.length > 0) {
+                    return;
+                }
+
+                this.clear();
+            }
+
             this.model = null;
             this.placeholder = 'Selecione uma opção';
-            this.selecteds = null;
+            this.selecteds = [];
             this.search = '';
             this.show = false;
+        },
+        get quantity() {
+            return this.selecteds?.length;
         },
         get options () {
             const availables = @js($options);
@@ -90,25 +144,34 @@
              role="combobox"
              aria-controls="options"
              aria-expanded="false">
-            <div x-on:click="show = !show" class="relative inset-y-0 left-0 flex w-full items-center space-x-2 overflow-hidden rounded-lg pl-2 transition">
+            <div x-on:click="show = true" class="relative inset-y-0 left-0 flex w-full items-center overflow-hidden rounded-lg pl-2 transition space-x-2">
                 <template x-if="multiple">
-                    <div>
+                    <div class="flex gap-2">
+                        <template x-if="quantity === 0">
+                            <span @class(['truncate font-medium', 'text-red-500' => $error]) x-bind:class="{ 'text-gray-400': selecteds }" x-text="placeholder"></span>
+                        </template>
                         <!-- count -->
-                        <span>2</span>
+                        <template x-if="quantity > 0">
+                            <span x-text="quantity"></span>
+                        </template>
                         <!-- badges -->
-                        <span class="gap-x- inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
-                            A
-                            <svg class="h-4 w-4 text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                            </svg>
-                        </span>
+                        <template wire:ignore x-for="(selected, index) in selecteds" :key="selected[selectable.label] ?? selected">
+                            <a href="#" class="cursor-pointer" x-on:click="clear(selected);">
+                                <div class="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+                                    <span x-text="selected[selectable.label]"></span>
+                                    <svg class="h-4 w-4 text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                </div>
+                            </a>
+                        </template>
                     </div>
                 </template>
                 <template x-if="!multiple">
                     <span @class(['truncate font-medium', 'text-red-500' => $error]) x-bind:class="{ 'text-gray-400': !selecteds }" x-text="placeholder"></span>
                 </template>
             </div>
-            <div class="flex items-center mr-1">
+            <div class="mr-1 flex items-center">
                 <!-- clean up button -->
                 <template x-if="Object.keys(selecteds ?? []).length > 0">
                     <button type="button" x-on:click="clear()">
@@ -127,7 +190,7 @@
                     <input type="text"
                            name="account-number"
                            id="account-number"
-                           class="block w-full rounded-md border-0 py-1.5 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                           class="block w-full rounded-md border-0 pr-10 placeholder:text-gray-400 text-gray-900 ring-1 ring-inset ring-gray-300 py-1.5 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                            placeholder="Procure alguma coisa"
                            x-model.debounce.500ms="search"
                     />
@@ -135,7 +198,7 @@
             </template>
             <template x-for="option in options" :key="option[selectable.label] ?? option">
                 <li x-on:click="select(option)"
-                    class="relative cursor-pointer select-none py-2 pr-2 pl-3 text-gray-700 hover:bg-gray-100 transition"
+                    class="relative cursor-pointer select-none py-2 pr-2 pl-3 text-gray-700 transition hover:bg-gray-100"
                     id="option-0"
                     role="option"
                     tabindex="-1"
@@ -143,7 +206,7 @@
                 >
                     <div wire:ignore class="flex items-center justify-between">
                         <span class="ml-2 truncate" x-text="option[selectable.label] ?? option"></span>
-                        <svg x-show="selected(option)" class="w-5 h-5 text-green-500 font-bold" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                        <svg x-show="selected(option)" class="h-5 w-5 font-bold text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"/>
                         </svg>
                     </div>
@@ -151,7 +214,7 @@
             </template>
             <template x-if="options.length === 0">
                 <li class="m-2">
-                    <span class="block w-full text-gray-700 pr-2">
+                    <span class="block w-full pr-2 text-gray-700">
                         Nenhum resultado encontrado
                     </span>
                 </li>
