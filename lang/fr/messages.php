5fc923c9295ed30132345bdd551ffde3d509ca95
Hitalo Ramon <hitaloramon@live.com>
2023-09-22 22:05:07 -0300

    added french language

---
+++ lang/fr/messages.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'errors' => [
+        'title' => 'Il y a :count erreurs de validation:',
+    ],
+
+    'select' => [
+        'placeholder' => 'Choisir une option',
+        'input' => 'Cherchez quelque chose ici',
+        'empty' => 'Aucun résultat trouvé',
+    ],
+
+    'dialog' => [
+        'button' => [
+            'ok' => 'OK',
+        ],
+    ],
+];
