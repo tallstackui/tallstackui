e6f752f5fd2636bb62fbd214ad2eb984ba444ca2
Hitalo Ramon <hitaloramon@live.com>
2023-09-22 21:58:32 -0300

    added spanish language

---
+++ lang/es/messages.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'errors' => [
+        'title' => 'Hay :count errores de validación:',
+    ],
+
+    'select' => [
+        'placeholder' => 'Seleccione una opción',
+        'input' => 'Busca algo aquí',
+        'empty' => 'No se encontraron resultados',
+    ],
+
+    'dialog' => [
+        'button' => [
+            'ok' => 'OK',
+        ],
+    ],
+];
