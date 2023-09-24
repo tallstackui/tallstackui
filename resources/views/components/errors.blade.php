8e113a78043d9a5a700f8f723e7018d1ea7d509c
AJ Meireles <alvaro.meireles@live.com>
2023-09-23 21:20:09 -0300

    refactoring errors

--- resources/views/components/errors.blade.php
+++ resources/views/components/errors.blade.php
@@ -1,29 +1,13 @@
 @if (($count = $errors->count()) > 0)
-    <div class="mx-auto max-w-lg">
-        <div {{ $attributes->class([
-                'rounded-lg p-4',
-                'bg-red-50'    => $color === 'red',
-                'bg-yellow-50' => $color === 'yellow',
-            ]) }}>
-            <div @class([
-                    'flex items-center border-b-2 pb-3',
-                    'text-red-800 border-red-200'       => $color === 'red',
-                    'text-yellow-800 border-yellow-200' => $color === 'yellow',
-                ])>
-                <span @class([
-                        'text-sm font-semibold',
-                        'text-red-800'    => $color === 'red',
-                        'text-yellow-800' => $color === 'yellow'
-                    ])>
+    <div @class($baseClass())>
+        <div {{ $attributes->class($wrapperClass()) }}>
+            <div @class($titleWrapperClass())>
+                <span @class($titleClass())>
                     {{ __($title, ['count' => $count]) }}
                 </span>
             </div>
             <div class="mt-2 ml-5 pl-1">
-                <ul @class([
-                        'list-disc text-sm space-y-1 text-negative-700',
-                        'text-red-800'    => $color === 'red',
-                        'text-yellow-800' => $color === 'yellow'
-                    ])>
+                <ul @class($bodyClass())>
                     @foreach ($messages($errors) as $message)
                         <li>{{ head($message) }}</li>
                     @endforeach
--- src/View/Components/Errors.php
+++ src/View/Components/Errors.php
@@ -3,8 +3,10 @@
 namespace TasteUi\View\Components;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Arr;
 use Illuminate\Support\ViewErrorBag;
 use Illuminate\View\Component;
+use TasteUi\Facades\TasteUi;
 
 class Errors extends Component
 {
@@ -19,6 +21,54 @@ class Errors extends Component
     public function render(): View
     {
         return view('taste-ui::components.errors');
+    }
+
+    public function baseClass(): string
+    {
+        return 'p-4 w-full';
+    }
+
+    public function wrapperClass(): string
+    {
+        return Arr::toCssClasses([
+            'rounded-lg p-4',
+            TasteUi::colors()
+                ->set('bg', $this->color, 50)
+                ->get(),
+        ]);
+    }
+
+    public function titleClass(): string
+    {
+        return Arr::toCssClasses([
+            'text-sm font-semibold',
+            TasteUi::colors()
+                ->set('text', $this->color, 800)
+                ->get(),
+        ]);
+    }
+
+    public function titleWrapperClass(): string
+    {
+        return Arr::toCssClasses([
+            'flex items-center border-b-2 pb-3',
+            TasteUi::colors()
+                ->set('text', $this->color, 800)
+                ->get(),
+            TasteUi::colors()
+                ->set('border', $this->color, 200)
+                ->get(),
+        ]);
+    }
+
+    public function bodyClass(): string
+    {
+        return Arr::toCssClasses([
+            'list-disc text-sm space-y-1 text-negative-700',
+            TasteUi::colors()
+                ->set('text', $this->color, 800)
+                ->get(),
+        ]);
     }
 
     public function messages(ViewErrorBag $errors): array
