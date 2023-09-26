df62b50c170ddb68ed7f817a5671fa92044cc309
AJ Meireles <alvaro.meireles@live.com>
2023-09-26 15:21:29 -0300

    fixing tests

--- tests/Browser/Alert/IndexTest.php
+++ tests/Browser/Alert/IndexTest.php
@@ -2,23 +2,33 @@
 
 namespace Tests\Browser\Alert;
 
-use Livewire\Component;
 use Laravel\Dusk\Browser;
+use Livewire\Component;
+use TasteUi\Facades\TasteUi;
 use Tests\Browser\BrowserTestCase;
 
 class IndexTest extends BrowserTestCase
 {
     /** @test */
-    public function can_close()
+    public function can_close(): void
     {
-        $this->browse(
-            fn (Browser $browser) => $this
-                ->visit($browser, AlertComponent::class)
+        $this->browse(function (Browser $browser) {
+            $this->visit($browser, AlertComponent::class)
                 ->assertSee('Foo bar')
                 ->click('#close')
-                ->pause(1000)
-                ->assertDontSee('Foo bar')
-        );
+                ->pause(500)
+                ->assertDontSee('Foo bar');
+        });
+    }
+
+    /** @test */
+    public function can_personalize(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $this->visit($browser, AlertPersonalizedComponent::class);
+            $this->assertNotNull($browser->element('.bg-red-500'));
+            $this->assertNull($browser->element('.bg-primary-500'));
+        });
     }
 }
 
@@ -26,9 +36,24 @@ class AlertComponent extends Component
 {
     public function render(): string
     {
-        return <<<HTML
+        return <<<'HTML'
         <div>
             <x-alert closeable>Foo bar</x-alert>
+        </div>
+HTML;
+    }
+}
+
+class AlertPersonalizedComponent extends Component
+{
+    public function render(): string
+    {
+        TasteUi::personalization('taste-ui::personalizations.alert')
+            ->block('main', fn () => 'rounded-md p-6 bg-red-500');
+
+        return <<<'HTML'
+        <div>
+            <x-alert>Foo bar</x-alert>
         </div>
 HTML;
     }
