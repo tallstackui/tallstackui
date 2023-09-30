f7b2cea855829b175642cc57e049100bc07864ec
AJ <aj@MacMini.local>
2023-09-30 00:48:38 -0300

    dialog personalization

---
+++ src/Support/Personalizations/Components/Interactions/Dialog.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace TasteUi\Support\Personalizations\Components;
+
+use Illuminate\Contracts\Support\Arrayable;
+use TasteUi\Support\Personalizations\Contracts\Personalizable;
+use TasteUi\Support\Personalizations\Traits\ShareablePersonalization;
+
+class Alert implements Arrayable, Personalizable
+{
+    use ShareablePersonalization;
+
+    public const EDITABLES = [
+        'base',
+        'title.base',
+        'title.wrapper',
+        'title.icon.wrapper',
+        'title.icon.classes',
+        'text.wrapper',
+        'text.title.wrapper',
+        'text.title.icon.wrapper',
+        'text.title.icon.classes',
+    ];
+}
