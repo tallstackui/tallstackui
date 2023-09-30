d7c2d734560628e361a474350dc8306dfe555705
AJ <alvaro.meireles@live.com>
2023-09-30 18:00:34 -0300

    personalizing

---
+++ src/Support/Personalizations/Components/Interactions/Toast.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace TasteUi\Support\Personalizations\Components\Interactions;
+
+use Illuminate\Contracts\Support\Arrayable;
+use TasteUi\Support\Personalizations\Contracts\Personalizable;
+use TasteUi\Support\Personalizations\Traits\ShareablePersonalization;
+
+class Dialog implements Arrayable, Personalizable
+{
+    use ShareablePersonalization;
+
+    public const EDITABLES = [
+        'background',
+        'wrapper.first',
+        'wrapper.second',
+        'wrapper.third',
+        'icon.wrapper',
+        'icon.size',
+        'text.wrapper',
+        'text.title',
+        'text.description.wrapper',
+        'text.description.text',
+        'buttons.wrapper',
+        'buttons.cancel',
+        'buttons.confirm',
+    ];
+
+    public function component(): string
+    {
+        return \TasteUi\View\Components\Interactions\Dialog::class;
+    }
+}
