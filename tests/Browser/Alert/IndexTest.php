000f151cdb4f1e9f6aa8038cc95965cb6cc81431
AJ <alvaro.meireles@live.com>
2023-10-02 18:12:55 -0300

    removing unnecessary test from alert

--- tests/Browser/Alert/IndexTest.php
+++ tests/Browser/Alert/IndexTest.php
@@ -3,9 +3,7 @@
 namespace Tests\Browser\Alert;
 
 use Laravel\Dusk\Browser;
-use Tests\Browser\Alert\Components\AlertCommonPersonalizedComponent;
 use Tests\Browser\Alert\Components\AlertComponent;
-use Tests\Browser\Alert\Components\AlertCustomPersonalizedComponent;
 use Tests\Browser\BrowserTestCase;
 
 class IndexTest extends BrowserTestCase
@@ -18,26 +16,6 @@ class IndexTest extends BrowserTestCase
                 ->assertSee('Foo bar')
                 ->click('#close')
                 ->waitUntilMissingText('Foo bar');
-        });
-    }
-
-    /** @test */
-    public function can_personalize(): void
-    {
-        $this->browse(function (Browser $browser) {
-            $this->visit($browser, AlertCommonPersonalizedComponent::class);
-            $this->assertNotNull($browser->element('.bg-red-500'));
-            $this->assertNull($browser->element('.bg-primary-500'));
-        });
-    }
-
-    /** @test */
-    public function can_custom_personalize(): void
-    {
-        $this->browse(function (Browser $browser) {
-            $this->visit($browser, AlertCustomPersonalizedComponent::class);
-            $this->assertNotNull($browser->element('.bg-red-500'));
-            $this->assertNull($browser->element('.bg-primary-500'));
         });
     }
 }
