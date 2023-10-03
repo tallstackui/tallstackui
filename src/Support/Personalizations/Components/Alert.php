3010312d30326aefb149aa54ada1ba5212d3d981
Wandes Cardoso <wandes2030@gmail.com>
2023-10-03 12:48:06 -0300

    Add dockBlock and extends Resource

--- src/Support/Personalizations/Components/Alert.php
+++ src/Support/Personalizations/Components/Alert.php
@@ -2,15 +2,25 @@
 
 namespace TasteUi\Support\Personalizations\Components;
 
+use Closure;
+use TasteUi\Contracts\Personalizable as Customizable;
 use TasteUi\Support\Personalizations\Contracts\Personalizable;
-use TasteUi\Support\Personalizations\Traits\ShareablePersonalization;
 use TasteUi\View\Components\Alert as Component;
 
-class Alert implements Personalizable
+/**
+ * @method $this base(string|Closure|Customizable $code)
+ * @method $this titleBase(string|Closure|Customizable $code)
+ * @method $this titleWrapper(string|Closure|Customizable $code)
+ * @method $this titleIconWrapper(string|Closure|Customizable $code)
+ * @method $this titleIconClasses(string|Closure|Customizable $code)
+ * @method $this textWrapper(string|Closure|Customizable $code)
+ * @method $this textTitleWrapper(string|Closure|Customizable $code)
+ * @method $this textTitleIconWrapper(string|Closure|Customizable $code)
+ * @method $this textTitleIconClasses(string|Closure|Customizable $code)
+ */
+class Alert extends Resource implements Personalizable
 {
-    use ShareablePersonalization;
-
-    public function component(): string
+    protected function component(): string
     {
         return Component::class;
     }
