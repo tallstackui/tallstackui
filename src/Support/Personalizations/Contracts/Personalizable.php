fa5fcee32bbf13dc9c90d7f6a477998d85fe1fb3
Wandes Cardoso <wandes2030@gmail.com>
2023-10-03 12:51:33 -0300

    remove unnecessary methods

--- src/Support/Personalizations/Contracts/Personalizable.php
+++ src/Support/Personalizations/Contracts/Personalizable.php
@@ -10,16 +10,6 @@ interface Personalizable
     public function get(string $block): ?string;
 
     /**
-     * Set the content of a block.
-     */
-    public function set(string $block, string $content): void;
-
-    /**
-     * Get the component class.
-     */
-    public function component(): string;
-
-    /**
      * Get the content of a block as array.
      */
     public function toArray(): array;
