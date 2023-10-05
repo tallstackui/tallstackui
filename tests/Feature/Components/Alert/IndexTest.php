4e27ef521631b01434273dbedbc3b4edf3290adb
AJ <alvaro.meireles@live.com>
2023-10-05 12:36:54 -0300

    wip - tests

--- tests/Feature/Components/Alert/IndexTest.php
+++ tests/Feature/Components/Alert/IndexTest.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\View\ViewException;
-use TasteUi\Facades\TasteUi;
 
 it('can render title', function () {
     $this->blade('<x-alert title="Foo bar" />')
@@ -74,65 +72,3 @@ test('when outline and white, it does not render outline', function () {
 it('can not render outline and translucent on the same component', function () {
     $this->blade('<x-alert text="Foo bar" outline translucent />');
 })->throws(ViewException::class);
-
-it('can personalize', function () {
-    $this->blade('<x-alert text="Bar foo" />')
-        ->assertSee('Bar foo')
-        ->assertSee('bg-primary-300')
-        ->assertDontSee('rounded-full');
-
-    TasteUi::personalize('alert')
-        ->block('base', function (array $data) {
-            return Arr::toCssClasses([
-                'rounded-full p-4',
-                TasteUi::colors()
-                    ->set('bg', $data['color'], 300)
-                    ->get() => ! $data['translucent'],
-                TasteUi::colors()
-                    ->set('bg', $data['color'], 100)
-                    ->get() => $data['translucent'],
-                TasteUi::colors()
-                    ->set('border', $data['color'], 900)
-                    ->get() => $data['outline'],
-                'border' => $data['outline'],
-            ]);
-        });
-
-    $this->blade('<x-alert text="Bar foo" />')
-        ->assertSee('Bar foo')
-        ->assertSee('bg-primary-300')
-        ->assertSee('rounded-full')
-        ->assertDontSee('rounded-md');
-});
-
-it('cannot personalize wrong block', function () {
-    $this->expectException(InvalidArgumentException::class);
-
-    $this->blade('<x-alert text="Bar foo" />')
-        ->assertSee('Bar foo')
-        ->assertSee('bg-primary-300')
-        ->assertDontSee('rounded-full');
-
-    TasteUi::personalize('alert')
-        ->block('foo-bar', function (array $data) {
-            return Arr::toCssClasses([
-                'rounded-full p-4',
-                TasteUi::colors()
-                    ->set('bg', $data['color'], 300)
-                    ->get() => ! $data['translucent'],
-                TasteUi::colors()
-                    ->set('bg', $data['color'], 100)
-                    ->get() => $data['translucent'],
-                TasteUi::colors()
-                    ->set('border', $data['color'], 900)
-                    ->get() => $data['outline'],
-                'border' => $data['outline'],
-            ]);
-        });
-
-    $this->blade('<x-alert text="Bar foo" />')
-        ->assertSee('Bar foo')
-        ->assertSee('bg-primary-300')
-        ->assertSee('rounded-full')
-        ->assertDontSee('rounded-md');
-});
--- tests/Feature/Components/Card/IndexTest.php
+++ tests/Feature/Components/Card/IndexTest.php
@@ -1,7 +1,5 @@
 <?php
 
-use TasteUi\Facades\TasteUi;
-
 it('can render', function () {
     $this->blade('<x-card>Foo bar</x-card>')
         ->assertSee('Foo bar');
@@ -24,34 +22,4 @@ it('can render with title and footer', function () {
         ->assertSee('Foo bar')
         ->assertSee('Lorem Ipsum')
         ->assertSee('Bar Baz');
-});
-
-it('can personalize', function () {
-    $this->blade('<x-card title="Lorem Ipsum" footer="Bar Baz">Foo bar</x-card>')
-        ->assertSee('rounded-b-xl');
-
-    TasteUi::personalize('card')
-        ->block('base', function () {
-            return 'grow rounded-t-xl px-2 py-5 text-secondary-700 md:px-4';
-        });
-
-    $this->blade('<x-card title="Lorem Ipsum" footer="Bar Baz">Foo bar</x-card>')
-        ->assertSee('rounded-t-xl')
-        ->assertDontSee('rounded-b-xl');
-});
-
-it('cannot personalize wrong block', function () {
-    $this->expectException(InvalidArgumentException::class);
-
-    $this->blade('<x-card title="Lorem Ipsum" footer="Bar Baz">Foo bar</x-card>')
-        ->assertSee('rounded-b-xl');
-
-    TasteUi::personalize('card')
-        ->block('foo-bar', function () {
-            return 'grow rounded-t-xl px-2 py-5 text-secondary-700 md:px-4';
-        });
-
-    $this->blade('<x-card title="Lorem Ipsum" footer="Bar Baz">Foo bar</x-card>')
-        ->assertSee('rounded-t-xl')
-        ->assertDontSee('rounded-b-xl');
 });
