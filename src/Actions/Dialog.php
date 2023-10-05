9adf30473748220a2ac5e9695e471f09b04b6285
AJ <alvaro.meireles@live.com>
2023-10-05 15:01:14 -0300

    Interactions tests

--- src/Actions/Dialog.php
+++ src/Actions/Dialog.php
@@ -6,13 +6,6 @@ class Dialog extends AbstractInteraction
 {
     protected string $event = 'tasteui:dialog';
 
-    public function time(int $time): self
-    {
-        $this->time = $time;
-
-        return $this;
-    }
-
     public function success(string $title, string $description = null): AbstractInteraction
     {
         return $this->base($title, $description, 'success');
