3e5e91658098a96f0ae714d181b7f488e52f7f74
AJ <alvaro.meireles@live.com>
2023-10-08 16:08:54 -0300

    uncloseable modal fix

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -28,7 +28,7 @@ class="relative {{ $zIndex }}"
     <div @class($customize['wrapper.second'])>
         <div @class($customize['wrapper.third'])>
             <div x-show="show"
-                 @if ($uncloseable) x-on:click.outside="show = false" @endif
+                 @if (!$uncloseable) x-on:click.outside="show = false" @endif
                  x-transition:enter="ease-out duration-300"
                  x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                  x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
