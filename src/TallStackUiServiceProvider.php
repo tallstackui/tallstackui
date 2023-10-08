26c201d2c8a8b361ec9e1b2156089dff3cac6c5c
AJ <alvaro.meireles@live.com>
2023-10-08 20:31:54 -0300

    moving config to src folder

--- src/TallStackUiServiceProvider.php
+++ src/TallStackUiServiceProvider.php
@@ -29,11 +29,11 @@ class TallStackUiServiceProvider extends ServiceProvider
     public function registerConfig(): void
     {
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'tallstack-ui');
-        $this->mergeConfigFrom(__DIR__.'/../config/tallstackui.php', 'tallstackui');
+        $this->mergeConfigFrom(__DIR__.'/config/tallstackui.php', 'tallstackui');
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadTranslationsFrom(__DIR__.'/../lang', 'tallstack-ui');
 
-        $this->publishes([__DIR__.'/../config/tallstackui.php' => config_path('tallstackui.php')], 'tallstackui.config');
+        $this->publishes([__DIR__.'/config/tallstackui.php' => config_path('tallstackui.php')], 'tallstackui.config');
         $this->publishes([__DIR__.'/../lang' => lang_path('vendor/tallstack-ui')], 'tallstackui.lang');
         $this->publishes([__DIR__.'/../resources/views' => resource_path('views/vendor/tallstack-ui')], 'tallstackui.views');
     }
