3a9c9a14d231f5ce085ce838de2aa9e1d5632249
AJ <alvaro.meireles@live.com>
2023-10-08 19:14:03 -0300

    feature test to tabs wire:model

--- tests/Feature/Components/Tabs/TabsTest.php
+++ tests/Feature/Components/Tabs/TabsTest.php
@@ -17,6 +17,23 @@ HTML;
         ->assertSee('Bar');
 });
 
+it('can render entangled', function () {
+    $component = <<<'HTML'
+    <x-tabs :options="['Foo', 'Bar']" wire:entangle="Foo">
+        <x-tabs.items tab="A">
+            Foo
+        </x-tabs.items>
+        <x-tabs.items tab="B">
+            Bar
+        </x-tabs.items>
+    </x-tabs>
+HTML;
+
+    $this->blade($component)
+        ->assertSee('Foo')
+        ->assertSee('Bar');
+});
+
 it('can render rounded', function () {
     $component = <<<'HTML'
     <x-tabs :options="['Foo', 'Bar']" selected="Foo">
