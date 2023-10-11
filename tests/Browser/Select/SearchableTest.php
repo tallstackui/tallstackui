7f1f1ff633adff25b345b2a40e22b8aa422ae1eb
AJ <alvaro.meireles@live.com>
2023-10-10 21:19:00 -0300

    fixing tests

--- tests/Browser/Select/SearchableTest.php
+++ tests/Browser/Select/SearchableTest.php
@@ -5,7 +5,6 @@ namespace Tests\Browser\Select;
 use Laravel\Dusk\Browser;
 use Tests\Browser\BrowserTestCase;
 use Tests\Browser\Select\Components\Searchable\SearchableAfterComponent;
-use Tests\Browser\Select\Components\Searchable\SearchableBeforeComponent;
 use Tests\Browser\Select\Components\Searchable\SearchableComponent;
 use Tests\Browser\Select\Components\Searchable\SearchableFilteredComponent;
 use Tests\Browser\Select\Components\Searchable\SearchableMultipleComponent;
@@ -19,7 +18,7 @@ class SearchableTest extends BrowserTestCase
             $this->visit($browser, SearchableComponent::class)
                 ->assertSee('Select an option')
                 ->assertDontSee('delectus aut autem')
-                ->click('#tallstackui_select_open_close')
+                ->click('@tallstackui_select_open_close')
                 ->waitForText('delectus aut autem');
         });
     }
@@ -33,12 +32,12 @@ class SearchableTest extends BrowserTestCase
                 ->assertDontSee('delectus aut autem')
                 ->assertDontSee('et porro tempora')
                 ->assertDontSee('quis ut nam facilis et officia qui')
-                ->click('#tallstackui_select_open_close')
+                ->click('@tallstackui_select_open_close')
                 ->waitForText('delectus aut autem')
                 ->assertSee('delectus aut autem')
                 ->assertSee('et porro tempora')
                 ->assertSee('quis ut nam facilis et officia qui')
-                ->type('#tallstackui_select_search_input', 'porro')
+                ->type('@tallstackui_select_search_input', 'porro')
                 ->waitUntilMissingText('delectus aut autem')
                 ->waitForText('et porro tempora')
                 ->waitUntilMissingText('quis ut nam facilis et officia qui');
@@ -52,11 +51,11 @@ class SearchableTest extends BrowserTestCase
             $this->visit($browser, SearchableFilteredComponent::class)
                 ->assertSee('Select an option')
                 ->assertDontSee('delectus aut autem')
-                ->click('#tallstackui_select_open_close')
+                ->click('@tallstackui_select_open_close')
                 ->waitForText('delectus aut autem')
                 ->clickAtXPath('/html/body/div[3]/div/div[2]/div[2]/ul/li[1]')
                 ->waitForText('delectus aut autem')
-                ->click('#tallstackui_select_clear')
+                ->click('@tallstackui_select_clear')
                 ->assertDontSee('delectus aut autem')
                 ->assertSee('Select an option');
         });
@@ -69,7 +68,7 @@ class SearchableTest extends BrowserTestCase
             $this->visit($browser, SearchableComponent::class)
                 ->assertSee('Select an option')
                 ->assertDontSee('laboriosam mollitia et enim quasi adipisci quia provident illum')
-                ->click('#tallstackui_select_open_close')
+                ->click('@tallstackui_select_open_close')
                 ->waitForText('laboriosam mollitia et enim quasi adipisci quia provident illum')
                 ->clickAtXPath('/html/body/div[3]/div/div[2]/div[2]/ul/li[5]')
                 ->click('#sync')
@@ -85,12 +84,12 @@ class SearchableTest extends BrowserTestCase
                 ->assertSee('Select an option')
                 ->assertDontSee('delectus aut autem')
                 ->assertDontSee('quis ut nam facilis et officia qui')
-                ->click('#tallstackui_select_open_close')
+                ->click('@tallstackui_select_open_close')
                 ->waitForText('delectus aut autem')
                 ->waitForText('quis ut nam facilis et officia qui')
                 ->clickAtXPath('/html/body/div[3]/div/div[2]/div[2]/ul/li[1]')
                 ->clickAtXPath('/html/body/div[3]/div/div[2]/div[2]/ul/li[2]')
-                ->click('#tallstackui_select_open_close')
+                ->click('@tallstackui_select_open_close')
                 ->click('#sync')
                 ->waitForText('[1,2]');
         });
@@ -102,27 +101,13 @@ class SearchableTest extends BrowserTestCase
         $this->browse(function (Browser $browser) {
             $this->visit($browser, SearchableAfterComponent::class)
                 ->assertSee('Select an option')
-                ->click('#tallstackui_select_open_close')
-                ->waitForText('After Slot')
-                ->waitForText('laboriosam mollitia et enim quasi adipisci quia provident illum')
-                ->clickAtXPath('/html/body/div[3]/div/div[2]/div[2]/ul/li[5]')
-                ->click('#sync')
-                ->waitForText('5');
-        });
-    }
-
-    /** @test */
-    public function can_render_before_slot(): void
-    {
-        $this->browse(function (Browser $browser) {
-            $this->visit($browser, SearchableBeforeComponent::class)
-                ->assertSee('Select an option')
-                ->click('#tallstackui_select_open_close')
-                ->waitForText('Before Slot')
-                ->waitForText('laboriosam mollitia et enim quasi adipisci quia provident illum')
-                ->clickAtXPath('/html/body/div[3]/div/div[2]/div[2]/ul/li[5]')
-                ->click('#sync')
-                ->waitForText('5');
+                ->click('@tallstackui_select_open_close')
+                ->type('@tallstackui_select_search_input', 'porro')
+                ->clickAtXPath('/html/body/div[3]/div/div[2]/div[2]/ul/li[1]')
+                ->click('@tallstackui_select_open_close')
+                ->type('@tallstackui_select_search_input', 'foo-bar-baz')
+                ->waitForText('No results found')
+                ->waitForText('After Slot');
         });
     }
 }
--- tests/Browser/Select/StyledTest.php
+++ tests/Browser/Select/StyledTest.php
@@ -5,7 +5,6 @@ namespace Tests\Browser\Select;
 use Laravel\Dusk\Browser;
 use Tests\Browser\BrowserTestCase;
 use Tests\Browser\Select\Components\Styled\StyledAfterComponent;
-use Tests\Browser\Select\Components\Styled\StyledBeforeComponent;
 use Tests\Browser\Select\Components\Styled\StyledComponent;
 use Tests\Browser\Select\Components\Styled\StyledMultipleComponent;
 use Tests\Browser\Select\Components\Styled\StyledSearchableComponent;
@@ -19,12 +18,12 @@ class StyledTest extends BrowserTestCase
             $this->visit($browser, StyledComponent::class)
                 ->assertSee('Select an option')
                 ->assertDontSee('bar')
-                ->click('#tallstackui_select_open_close')
+                ->click('@tallstackui_select_open_close')
                 ->waitForText('foo')
                 ->clickAtXPath('/html/body/div[3]/div/div[2]/div[2]/ul/li[1]')
                 ->click('#sync')
                 ->waitForText('bar')
-                ->click('#tallstackui_select_clear')
+                ->click('@tallstackui_select_clear')
                 ->click('#sync')
                 ->waitUntilMissingText('bar');
         });
@@ -37,7 +36,7 @@ class StyledTest extends BrowserTestCase
             $this->visit($browser, StyledComponent::class)
                 ->assertSee('Select an option')
                 ->assertDontSee('bar')
-                ->click('#tallstackui_select_open_close')
+                ->click('@tallstackui_select_open_close')
                 ->waitForText('foo')
                 ->clickAtXPath('/html/body/div[3]/div/div[2]/div[2]/ul/li[1]')
                 ->click('#sync')
@@ -52,9 +51,9 @@ class StyledTest extends BrowserTestCase
             $this->visit($browser, StyledSearchableComponent::class)
                 ->assertSee('Select an option')
                 ->assertDontSee('foo')
-                ->click('#tallstackui_select_open_close')
+                ->click('@tallstackui_select_open_close')
                 ->waitForText('foo')
-                ->type('#tallstackui_select_search_input', 'bar')
+                ->type('@tallstackui_select_search_input', 'bar')
                 ->clickAtXPath('/html/body/div[3]/div/div[2]/div[2]/ul/li[1]')
                 ->click('#sync')
                 ->waitForText('foo');
@@ -69,12 +68,12 @@ class StyledTest extends BrowserTestCase
                 ->assertSee('Select an option')
                 ->assertDontSee('foo')
                 ->assertDontSee('bar')
-                ->click('#tallstackui_select_open_close')
+                ->click('@tallstackui_select_open_close')
                 ->waitForText('foo')
                 ->waitForText('bar')
                 ->clickAtXPath('/html/body/div[3]/div/div[2]/div[2]/ul/li[1]')
                 ->clickAtXPath('/html/body/div[3]/div/div[2]/div[2]/ul/li[2]')
-                ->click('#tallstackui_select_open_close')
+                ->click('@tallstackui_select_open_close')
                 ->click('#sync')
                 ->waitForText('["bar","foo"]');
         });
@@ -87,36 +86,15 @@ class StyledTest extends BrowserTestCase
             $this->visit($browser, StyledAfterComponent::class)
                 ->assertSee('Select an option')
                 ->assertDontSee('bar')
-                ->click('#tallstackui_select_open_close')
+                ->click('@tallstackui_select_open_close')
                 ->waitForText('foo')
                 ->clickAtXPath('/html/body/div[3]/div/div[2]/div[2]/ul/li[1]')
                 ->click('#sync')
                 ->waitForText('bar')
-                ->click('#tallstackui_select_open_close')
-                ->type('#tallstackui_select_search_input', 'porro')
+                ->click('@tallstackui_select_open_close')
+                ->type('@tallstackui_select_search_input', 'porro')
                 ->waitForText('After Slot')
-                ->click('#tallstackui_select_clear')
-                ->click('#sync')
-                ->waitUntilMissingText('bar');
-        });
-    }
-
-    /** @test */
-    public function can_render_before_slot(): void
-    {
-        $this->browse(function (Browser $browser) {
-            $this->visit($browser, StyledBeforeComponent::class)
-                ->assertSee('Select an option')
-                ->assertDontSee('bar')
-                ->click('#tallstackui_select_open_close')
-                ->waitForText('foo')
-                ->clickAtXPath('/html/body/div[3]/div/div[2]/div[2]/ul/li[1]')
-                ->click('#sync')
-                ->waitForText('bar')
-                ->click('#tallstackui_select_open_close')
-                ->type('#tallstackui_select_search_input', 'porro')
-                ->waitForText('Before Slot')
-                ->click('#tallstackui_select_clear')
+                ->click('@tallstackui_select_clear')
                 ->click('#sync')
                 ->waitUntilMissingText('bar');
         });
