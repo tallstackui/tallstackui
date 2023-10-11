624e5697f17466da32788fd68e880da1faeae5ce
AJ <alvaro.meireles@live.com>
2023-10-10 23:03:20 -0300

    fixing input padding on right

--- src/View/Components/Form/Traits/DefaultInputClasses.php
+++ src/View/Components/Form/Traits/DefaultInputClasses.php
@@ -9,16 +9,18 @@ trait DefaultInputClasses
 {
     private function tallStackUiInputClasses(): string
     {
-        $round = rescue(fn () => $this->round, report: false); // @phpstan-ignore-line
-        $textarea = $this instanceof Textarea;
+        $round = $this->round ?? null;
+        $icon = $this->icon ?? null;
+        $position = $this->position ?? null;
 
         return Arr::toCssClasses([
             'block w-full border-0 py-1.5 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 transition',
             'placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6',
             'disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200',
             'read-only:bg-gray-100 read-only:text-gray-500 read-only:ring-gray-200',
+            'pr-10' => $icon && $position === 'right',
             'rounded-md' => ! $this->square && ! $round,
-            'rounded-full' => $round && ! $textarea,
+            'rounded-full' => $round && ! $this instanceof Textarea,
         ]);
     }
 }
