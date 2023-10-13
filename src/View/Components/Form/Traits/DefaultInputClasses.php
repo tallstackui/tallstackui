eb7d1fe825224a6b261f6df6925766650d3c31d1
AJ <alvaro.meireles@live.com>
2023-10-12 22:53:32 -0300

    fixing input textarea auto-resize with validation error

--- src/View/Components/Form/Traits/DefaultInputClasses.php
+++ src/View/Components/Form/Traits/DefaultInputClasses.php
@@ -16,7 +16,7 @@ trait DefaultInputClasses
         return Arr::toCssClasses([
             'block w-full border-0 py-1.5 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 transition',
             'placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6',
-            'disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200',
+            'disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200 overflow-hidden',
             'read-only:bg-gray-100 read-only:text-gray-500 read-only:ring-gray-200',
             'pr-10' => $icon && $position === 'right',
             'rounded-md' => ! $this->square && ! $round,
