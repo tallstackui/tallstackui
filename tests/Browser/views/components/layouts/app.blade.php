4de6635cae9259575c86730d3e01fd3c468059e5
AJ <alvaro.meireles@live.com>
2023-10-18 15:28:28 -0300

    fixing tests

--- tests/Browser/views/components/layouts/app.blade.php
+++ tests/Browser/views/components/layouts/app.blade.php
@@ -2,7 +2,8 @@
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon">
-    <tallstackui:setup />
+    @tallStackUiScripts
+    @tallStackUiStyles
 </head>
 <body>
     <x-dialog />
