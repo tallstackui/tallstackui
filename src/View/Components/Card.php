6bb2476780c2acfa4af41da3363f65925f7414f7
AJ <alvaro.meireles@live.com>
2023-10-17 23:21:09 -0300

    modal

--- src/View/Components/Card.php
+++ src/View/Components/Card.php
@@ -24,12 +24,12 @@ class Card extends Component implements Personalize
                 'second' => 'dark:bg-dark-700 flex w-full flex-col rounded-lg bg-white shadow-md',
             ],
             'header' => [
-                'wrapper' => 'dark:border-b-dark-500 flex items-center justify-between border-b p-4',
+                'wrapper' => 'dark:border-b-dark-600 flex items-center justify-between border-b p-4',
                 'text' => 'text-md text-secondary-700 dark:text-dark-300 font-medium',
             ],
             'body' => 'text-secondary-700 dark:text-dark-300 grow rounded-b-xl px-2 py-5 md:px-4',
             'footer' => [
-                'wrapper' => 'bg-secondary-50 text-secondary-700 dark:text-dark-300 dark:border-t-dark-500 dark:bg-dark-600 rounded-lg rounded-t-none border-t p-4 sm:px-6',
+                'wrapper' => 'bg-secondary-50 text-secondary-700 dark:text-dark-300 dark:border-t-dark-600 dark:bg-dark-700 rounded-lg rounded-t-none border-t p-4 sm:px-6',
                 'text' => 'flex items-center justify-end gap-2',
             ],
         ]);
