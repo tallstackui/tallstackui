34552b8cff0757bb9fcdfeee9f3c01cfbe0ca5de
AJ <alvaro.meireles@live.com>
2023-10-18 16:15:54 -0300

    select styled with dark:bg-dark-800

--- src/View/Components/Wrapper/Select.php
+++ src/View/Components/Wrapper/Select.php
@@ -38,7 +38,7 @@ class Select extends Component implements Personalize
     {
         return Arr::dot([
             'input' => [
-                'wrapper' => 'flex w-full cursor-pointer items-center gap-x-2 rounded-md border-0 bg-white text-gray-600 py-1.5 shadow-sm ring-1 ring-inset text-sm leading-6 dark:text-dark-300 dark:bg-dark-700',
+                'wrapper' => 'flex w-full cursor-pointer items-center gap-x-2 rounded-md border-0 bg-white text-gray-600 py-1.5 shadow-sm ring-1 ring-inset text-sm leading-6 dark:text-dark-300 dark:bg-dark-800',
                 'error' => $this->error(),
             ],
             'header' => 'relative inset-y-0 left-0 flex w-full items-center overflow-hidden rounded-lg pl-2 transition space-x-2',
