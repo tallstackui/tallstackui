d588b7cd1d56c7a4bb8a9c82f0f056c40f7ad684
AJ <alvaro.meireles@live.com>
2023-10-18 16:21:11 -0300

    select styled placeholder in dark mode

--- resources/views/components/select/searchable.blade.php
+++ resources/views/components/select/searchable.blade.php
@@ -19,8 +19,11 @@
     <x-slot:header>
         <div class="flex gap-2">
             <template x-if="!multiple || quantity === 0">
-                <span @class(['truncate', 'text-red-500' => $error]) x-bind:class="{ 'text-gray-400': empty, 'text-gray-600': !empty }"
-                      x-text="placeholder"></span>
+                <span @class(['truncate', 'text-red-500' => $error])
+                      x-bind:class="{
+                        'text-gray-400 dark:text-dark-400': empty,
+                        'text-gray-600 dark:text-dark-300': !empty
+                      }" x-text="placeholder"></span>
             </template>
             <template x-if="multiple && quantity > 0">
                 <span x-text="quantity"></span>
