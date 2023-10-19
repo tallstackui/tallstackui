fc96832cce96c751978a2a7a375c3d46d4ad1fa4
Wallace A <andradewall0@gmail.com>
2023-10-19 10:37:07 -0300

    Add feature tests

--- tests/Feature/Components/Button/CircleTest.php
+++ tests/Feature/Components/Button/CircleTest.php
@@ -33,3 +33,33 @@ it('can render colored', function () {
     $this->blade('<x-button.circle text="Foo bar" color="yellow" />')->assertSee('bg-yellow-500');
     $this->blade('<x-button.circle text="Foo bar" color="blue" />')->assertSee('bg-blue-500');
 });
+
+it('can render large size as default', function () {
+    $this->blade('<x-button.circle text="LG" color="primary" />')
+        ->assertSee('w-12 h-12')
+        ->assertSee('text-2xl');
+
+    $this->blade('<x-button.circle icon="users" color="primary" />')
+        ->assertSee('w-12 h-12')
+        ->assertSee('w-8 h-8');
+});
+
+it('can render medium size', function () {
+    $this->blade('<x-button.circle size="md" text="MD" color="primary" />')
+        ->assertSee('w-9 h-9')
+        ->assertSee('text-base');
+
+    $this->blade('<x-button.circle size="md" icon="users" color="primary" />')
+        ->assertSee('w-9 h-9')
+        ->assertSee('w-4 h-4');
+});
+
+it('can render small size', function () {
+    $this->blade('<x-button.circle size="sm" text="MD" color="primary" />')
+        ->assertSee('w-6 h-6')
+        ->assertSee('text-xs');
+
+    $this->blade('<x-button.circle size="sm" icon="users" color="primary" />')
+        ->assertSee('w-6 h-6')
+        ->assertSee('w-3 h-3');
+});
