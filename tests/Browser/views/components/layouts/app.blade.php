f6f4a5445a1e0a9531237eba416a630860f6f207
AJ <alvaro.meireles@live.com>
2023-10-23 22:07:14 -0300

    wip

--- tests/Browser/views/components/layouts/app.blade.php
+++ tests/Browser/views/components/layouts/app.blade.php
@@ -1,7 +1,6 @@
 <html>
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon">
     @tallStackUiScripts
     @tallStackUiStyles
 </head>
