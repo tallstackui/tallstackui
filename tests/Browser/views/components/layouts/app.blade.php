40cd63908373501824244442b9eaf634bc334482
AJ <alvaro.meireles@live.com>
2023-10-23 22:09:17 -0300

    restoring layout structure

--- tests/Browser/views/components/layouts/app.blade.php
+++ tests/Browser/views/components/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <html>
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon">
     @tallStackUiScripts
     @tallStackUiStyles
 </head>
