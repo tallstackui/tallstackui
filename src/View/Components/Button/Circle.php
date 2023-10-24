7cc49a1fdcc5ace853333d183953f71560f2e266
AJ <alvaro.meireles@live.com>
2023-10-24 19:10:49 -0300

    enhancements

--- src/View/Components/Button/Circle.php
+++ src/View/Components/Button/Circle.php
@@ -3,6 +3,7 @@
 namespace TallStackUi\View\Components\Button;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Arr;
 use Illuminate\View\Component;
 use TallStackUi\View\Personalizations\Contracts\Personalize;
 use TallStackUi\View\Personalizations\Traits\InteractWithProviders;
@@ -10,12 +11,6 @@ use TallStackUi\View\Personalizations\Traits\InteractWithProviders;
 class Circle extends Component implements Personalize
 {
     use InteractWithProviders;
-
-    private array $buttonSizeClasses;
-
-    private array $fontSizeClasses;
-
-    private array $iconSizeClasses;
 
     public function __construct(
         public ?string $text = null,
@@ -27,38 +22,39 @@ class Circle extends Component implements Personalize
         public ?string $loading = null,
         public ?string $delay = null,
         public ?string $style = null,
-        public ?string $size = 'lg',
+        public ?string $sm = null,
+        public ?string $md = null,
+        public ?string $lg = null,
+        public ?string $size = null,
     ) {
         $this->style = $this->outline ? 'outline' : 'solid';
-
-        $this->buttonSizeClasses = [
-            'sm' => 'w-6 h-6',
-            'md' => 'w-9 h-9',
-            'lg' => 'w-12 h-12',
-        ];
-
-        $this->iconSizeClasses = [
-            'sm' => 'w-3 h-3',
-            'md' => 'w-4 h-4',
-            'lg' => 'w-8 h-8',
-        ];
-
-        $this->fontSizeClasses = [
-            'sm' => 'text-xs',
-            'md' => 'text-base',
-            'lg' => 'text-2xl',
-        ];
+        $this->size = $this->lg ? 'lg' : ($this->sm ? 'sm' : 'md');
 
         $this->colors();
     }
 
     public function personalization(): array
     {
-        return [
-            'wrapper' => $this->buttonSizeClasses[$this->size].' outline-none inline-flex justify-center items-center group transition ease-in duration-150 font-semibold focus:ring-2 focus:ring-offset-2 hover:shadow-sm disabled:opacity-50 disabled:cursor-not-allowed rounded-full',
-            'icon.size' => $this->iconSizeClasses[$this->size],
-            'span' => $this->fontSizeClasses[$this->size],
-        ];
+        return Arr::dot([
+            'wrapper' => [
+                'base' => 'outline-none inline-flex justify-center items-center group transition ease-in duration-150 font-semibold focus:ring-2 focus:ring-offset-2 hover:shadow-sm disabled:opacity-50 disabled:cursor-not-allowed rounded-full',
+                'sizes' => [
+                    'sm' => 'w-6 h-6',
+                    'md' => 'w-9 h-9',
+                    'lg' => 'w-12 h-12',
+                ]
+            ],
+            'icon.sizes' => [
+                'sm' => 'w-3 h-3',
+                'md' => 'w-4 h-4',
+                'lg' => 'w-6 h-6',
+            ],
+            'text.sizes' => [
+                'sm' => 'text-xs',
+                'md' => 'text-md',
+                'lg' => 'text-xl',
+            ],
+        ]);
     }
 
     public function render(): View
--- src/resources/views/components/button/button.blade.php
+++ src/resources/views/components/button/button.blade.php
@@ -1,65 +1,32 @@
 @php
     $tag = $href ? 'a' : 'button';
     $customize = tallstackui_personalization('button', $personalization());
+
+    $sizes['wrapper'] = $customize['wrapper.sizes.' . $size];
+    $sizes['icon'] = $customize['icon.sizes.' . $size];
 @endphp
 
 <{{ $tag }} @if ($href) href="{{ $href }}" @else
     role="button"
 @endif {{ $attributes->class([
         $customize['wrapper.class'],
-        $customize['wrapper.sizes.xs'] => $size === 'xs',
-        $customize['wrapper.sizes.sm'] => $size === 'sm',
-        $customize['wrapper.sizes.md'] => $size === 'md',
-        $customize['wrapper.sizes.lg'] => $size === 'lg',
+        $sizes['wrapper'],
         $colors['wrapper.color'],
         'rounded' => !$square && !$round,
         'rounded-full' => !$square && $round !== null,
-    ]) }}
-    wire:loading.attr="disabled"
-    wire:loading.class="!cursor-wait">
+    ]) }} wire:loading.attr="disabled" wire:loading.class="!cursor-wait">
     @if ($icon && $position === 'left')
-        <x-icon :$icon @class([
-                $customize['icon.sizes.xs'] => $size === 'xs',
-                $customize['icon.sizes.sm'] => $size === 'sm',
-                $customize['icon.sizes.md'] => $size === 'md',
-                $customize['icon.sizes.lg'] => $size === 'lg',
-                $colors['icon.color']
-            ]) />
+        <x-icon :$icon @class([$sizes['icon'], $colors['icon.color']]) />
     @endif
     {{ $text ?? $slot }}
     @if ($icon && $position === 'right')
-        <x-icon :$icon @class([
-                $customize['icon.sizes.xs'] => $size === 'xs',
-                $customize['icon.sizes.sm'] => $size === 'sm',
-                $customize['icon.sizes.md'] => $size === 'md',
-                $customize['icon.sizes.lg'] => $size === 'lg',
-                $colors['icon.color']
-            ]) />
+        <x-icon :$icon @class([$sizes['icon'], $colors['icon.color']]) />
     @endif
     @if ($loading)
-        <svg @if ($loading !== "1") wire:target="{{ $loading }}" @endif
-             wire:loading.delay{{ $delay ? ".{$delay}" : "" }}
-             @class([
-                'animate-spin',
-                $customize['icon.loading.sizes.xs'] => $size === 'xs',
-                $customize['icon.loading.sizes.sm'] => $size === 'sm',
-                $customize['icon.loading.sizes.md'] => $size === 'md',
-                $customize['icon.loading.sizes.lg'] => $size === 'lg',
-                $colors['icon.loading.color'],
-             ])
-             dusk="button-loading-spinner"
-             xmlns="http://www.w3.org/2000/svg"
-             fill="none"
-             viewBox="0 0 24 24">
-            <circle class="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    stroke-width="4"></circle>
-            <path class="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-        </svg>
+        <x-tallstack-ui::icon.others.loading-button :$loading :$delay @class([
+            'animate-spin',
+            $sizes['icon'],
+            $colors['icon.loading.color'],
+        ]) />
     @endif
 </{{ $tag }}>
