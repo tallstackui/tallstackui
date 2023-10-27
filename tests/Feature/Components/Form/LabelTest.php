7bbdaab9ccd0e31a8c0d3c94072967ff219d3082
AJ Meireles <alvaro.meireles@live.com>
2023-10-27 18:52:56 -0300

    format

--- tests/Feature/Components/Form/LabelTest.php
+++ tests/Feature/Components/Form/LabelTest.php
@@ -1,14 +1,13 @@
 <?php
 
 it('can render askerisk', function () {
-    $component = <<<HTML
+    $component = <<<'HTML'
     <x-input label="FooBar *" hint="Insert your name" />
     HTML;
 
     $this->blade($component)
         ->assertSee('<i class', false);
 });
-
 
 it('cannot render askerisk', function (string $label) {
     $component = <<<HTML
@@ -18,4 +17,3 @@ it('cannot render askerisk', function (string $label) {
     $this->blade($component)
         ->assertDontSee('<i class', false);
 })->with(['FooBar', 'FooBar **']);
-
