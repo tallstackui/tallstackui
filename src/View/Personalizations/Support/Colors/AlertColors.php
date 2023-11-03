b971b8c312dbef236135a6a910c120b5574ead8a
AJ <alvaro.meireles@live.com>
2023-11-03 14:42:11 -0300

    refactoring alert colors

--- src/View/Personalizations/Support/Colors/AlertColors.php
+++ src/View/Personalizations/Support/Colors/AlertColors.php
@@ -3,10 +3,8 @@
 namespace TallStackUi\View\Personalizations\Support\Colors;
 
 use Illuminate\Support\Arr;
-use TallStackUi\Facades\TallStackUi;
 use TallStackUi\View\Components\Alert;
 
-// TODO: refactor
 class AlertColors
 {
     public function __construct(protected Alert $component)
@@ -19,57 +17,140 @@ class AlertColors
         $text = $this->text();
 
         return [
-            'wrapper.color' => Arr::toCssClasses([$this->background(), 'border border-gray-100' => $this->component->color === 'white']),
+            'wrapper.color' => $this->background(),
             'title.base.color' => Arr::toCssClasses([$text => $this->component->title !== null]),
             'title.close.color' => $text,
             'text.title.wrapper.color' => $text,
             'text.close.color' => $text,
-            'icon.color' => Arr::toCssClasses([
-                TallStackUi::tailwind()
-                    ->set('text', $this->component->color === 'black' ? 'white' : $this->component->color, $this->component->color === 'black' ? null : 500)
-                    ->get() => $this->component->color !== 'white',
-            ]),
+            'icon.color' => $this->text(),
         ];
     }
 
     private function background(): string
     {
-        if ($this->component->style === 'solid') {
-            $weight = match ($this->component->color) {
-                'white', 'black' => null,
-                default => 300,
-            };
-        } else {
-            $weight = 100;
-        }
+        $colors = [
+            'solid' => [
+                'white' => 'bg-white border border-gray-100',
+                'black' => 'bg-black',
+                'primary' => 'bg-primary-300',
+                'secondary' => 'bg-secondary-300',
+                'slate' => 'bg-slate-300',
+                'gray' => 'bg-gray-300',
+                'zinc' => 'bg-zinc-300',
+                'neutral' => 'bg-neutral-300',
+                'stone' => 'bg-stone-300',
+                'red' => 'bg-red-300',
+                'orange' => 'bg-orange-300',
+                'amber' => 'bg-amber-300',
+                'yellow' => 'bg-yellow-300',
+                'lime' => 'bg-lime-300',
+                'green' => 'bg-green-300',
+                'emerald' => 'bg-emerald-300',
+                'teal' => 'bg-teal-300',
+                'cyan' => 'bg-cyan-300',
+                'sky' => 'bg-sky-300',
+                'blue' => 'bg-blue-300',
+                'indigo' => 'bg-indigo-300',
+                'violet' => 'bg-violet-300',
+                'purple' => 'bg-purple-300',
+                'fuchsia' => 'bg-fuchsia-300',
+                'pink' => 'bg-pink-300',
+                'rose' => 'bg-rose-300',
+            ],
+            'translucent' => [
+                'white' => 'bg-white border border-gray-100',
+                'black' => 'bg-black-100',
+                'primary' => 'bg-primary-100',
+                'secondary' => 'bg-secondary-100',
+                'slate' => 'bg-slate-100',
+                'gray' => 'bg-gray-100',
+                'zinc' => 'bg-zinc-100',
+                'neutral' => 'bg-neutral-100',
+                'stone' => 'bg-stone-100',
+                'red' => 'bg-red-100',
+                'orange' => 'bg-orange-100',
+                'amber' => 'bg-amber-100',
+                'yellow' => 'bg-yellow-100',
+                'lime' => 'bg-lime-100',
+                'green' => 'bg-green-100',
+                'emerald' => 'bg-emerald-100',
+                'teal' => 'bg-teal-100',
+                'cyan' => 'bg-cyan-100',
+                'sky' => 'bg-sky-100',
+                'blue' => 'bg-blue-100',
+                'indigo' => 'bg-indigo-100',
+                'violet' => 'bg-violet-100',
+                'purple' => 'bg-purple-100',
+                'fuchsia' => 'bg-fuchsia-100',
+                'pink' => 'bg-pink-100',
+                'rose' => 'bg-rose-100',
+            ],
+        ];
 
-        return TallStackUi::tailwind()
-            ->set('bg', $this->component->color, $weight)
-            ->get();
+        return $colors[$this->component->style][$this->component->color];
     }
 
     private function text(): string
     {
-        $weight = (match ($this->component->color) {
-            'black' => fn () => $this->component->style === 'solid' ? null : 700,
-            default => fn () => 900,
-        })();
+        $colors = [
+            'solid' => [
+                'white' => 'text-black',
+                'black' => 'text-white',
+                'primary' => 'text-primary-900',
+                'secondary' => 'text-secondary-900',
+                'slate' => 'text-slate-900',
+                'gray' => 'text-gray-900',
+                'zinc' => 'text-zinc-900',
+                'neutral' => 'text-neutral-900',
+                'stone' => 'text-stone-900',
+                'red' => 'text-red-900',
+                'orange' => 'text-orange-900',
+                'amber' => 'text-amber-900',
+                'yellow' => 'text-yellow-900',
+                'lime' => 'text-lime-900',
+                'green' => 'text-green-900',
+                'emerald' => 'text-emerald-900',
+                'teal' => 'text-teal-900',
+                'cyan' => 'text-cyan-900',
+                'sky' => 'text-sky-900',
+                'blue' => 'text-blue-900',
+                'indigo' => 'text-indigo-900',
+                'violet' => 'text-violet-900',
+                'purple' => 'text-purple-900',
+                'fuchsia' => 'text-fuchsia-900',
+                'pink' => 'text-pink-900',
+                'rose' => 'text-rose-900',
+            ],
+            'translucent' => [
+                'white' => 'text-black',
+                'black' => 'text-black-900',
+                'primary' => 'text-primary-900',
+                'secondary' => 'text-secondary-900',
+                'slate' => 'text-slate-900',
+                'gray' => 'text-gray-900',
+                'zinc' => 'text-zinc-900',
+                'neutral' => 'text-neutral-900',
+                'stone' => 'text-stone-900',
+                'red' => 'text-red-900',
+                'orange' => 'text-orange-900',
+                'amber' => 'text-amber-900',
+                'yellow' => 'text-yellow-900',
+                'lime' => 'text-lime-900',
+                'green' => 'text-green-900',
+                'emerald' => 'text-emerald-900',
+                'teal' => 'text-teal-900',
+                'cyan' => 'text-cyan-900',
+                'sky' => 'text-sky-900',
+                'blue' => 'text-blue-900',
+                'indigo' => 'text-indigo-900',
+                'violet' => 'text-violet-900',
+                'purple' => 'text-purple-900',
+                'fuchsia' => 'text-fuchsia-900',
+                'pink' => 'text-pink-900',
+                'rose' => 'text-rose-900',
+            ],
+        ];
 
-        if ($this->component->style === 'solid') {
-            $colors = match ($this->component->color) {
-                'black' => 'white',
-                'white' => 'neutral',
-                default => $this->component->color,
-            };
-        } else {
-            $colors = match ($this->component->color) {
-                'black', 'white' => 'neutral',
-                default => $this->component->color,
-            };
-        }
-
-        return TallStackUi::tailwind()
-            ->set('text', $colors, $weight)
-            ->get();
+        return $colors[$this->component->style][$this->component->color];
     }
 }
