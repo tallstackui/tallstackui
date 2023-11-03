5922316f89f3e45b4d0ce9439726c45c206c71c4
AJ <alvaro.meireles@live.com>
2023-11-03 18:41:26 -0300

    wip

--- tests/Browser/Button/IndexTest.php
+++ tests/Browser/Button/IndexTest.php
@@ -2,30 +2,71 @@
 
 namespace Tests\Browser\Button;
 
-use Laravel\Dusk\Browser;
+use Livewire\Component;
+use Livewire\Livewire;
 use Tests\Browser\BrowserTestCase;
-use Tests\Browser\Button\Components\ButtonComponent;
-use Tests\Browser\Button\Components\CircleComponent;
 
 class IndexTest extends BrowserTestCase
 {
     /** @test */
-    public function can_see_loading_spinner(): void
+    public function can_see_loading_spinner_with_circle_button(): void
     {
-        $this->browse(function (Browser $browser) {
-            $this->visit($browser, ButtonComponent::class)
-                ->assertDontSee('svg')
-                ->type('input', 'Foo bar')
-                ->click('@sync')
-                ->waitFor('@button-loading-spinner');
-        });
+        Livewire::visit(new class extends Component
+        {
+            public ?string $foo = null;
 
-        $this->browse(function (Browser $browser) {
-            $this->visit($browser, CircleComponent::class)
-                ->assertDontSee('svg')
-                ->type('input', 'Foo bar')
-                ->click('@sync')
-                ->waitFor('@button-loading-spinner');
-        });
+            public function render(): string
+            {
+                return <<<'HTML'
+                <div>
+                    <x-input id="input" wire:model="foo" />
+                
+                    <x-button.circle dusk="sync" loading="sync" wire:click="sync" text="Save" />
+                </div>
+                HTML;
+            }
+
+            public function sync(): void
+            {
+                sleep(1);
+
+                // ...
+            }
+        })
+            ->assertDontSee('svg')
+            ->type('input', 'Foo bar')
+            ->click('@sync')
+            ->waitFor('@button-loading-spinner');
+    }
+
+    /** @test */
+    public function can_see_loading_spinner_with_normal_button(): void
+    {
+        Livewire::visit(new class extends Component
+        {
+            public ?string $foo = null;
+
+            public function render(): string
+            {
+                return <<<'HTML'
+                <div>
+                    <x-input id="input" wire:model="foo" />
+                
+                    <x-button dusk="sync" loading="sync" wire:click="sync" text="Save" />
+                </div>
+                HTML;
+            }
+
+            public function sync(): void
+            {
+                sleep(1);
+
+                // ...
+            }
+        })
+            ->assertDontSee('svg')
+            ->type('input', 'Foo bar')
+            ->click('@sync')
+            ->waitFor('@button-loading-spinner');
     }
 }
