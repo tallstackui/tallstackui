3638242c09bb8b7082c3d0a275b98c42daaeac5b
AJ <alvaro.meireles@live.com>
2023-11-03 21:05:15 -0300

    wip

---
+++ src/View/Personalizations/Exceptions/InvalidSelectedColor.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TallStackUi\View\Personalizations\Exceptions;
+
+use Exception;
+
+class InvalidSelectedColor extends Exception
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
