6e1053c391094f68ad3baaa71e4ec4a0cee6b6ba
hitaloramon <hitaloramon@live.com>
2023-11-15 23:05:07 -0300

    fix rounded in tab component

--- src/View/Components/Tab/Tab.php
+++ src/View/Components/Tab/Tab.php
@@ -19,9 +19,9 @@ class Tab extends Component implements Personalization
     public function personalization(): array
     {
         return Arr::dot([
-            'wrapper' => '-mb-px flex items-stretch overflow-auto',
+            'wrapper' => '-mb-1.5 flex items-stretch overflow-auto',
             'item' => [
-                'wrapper' => 'inline-flex cursor-pointer justify-center truncate px-5 py-2.5 text-gray-700 transition rounded-t-lg',
+                'wrapper' => 'inline-flex cursor-pointer justify-center truncate px-5 py-3 text-gray-700 transition rounded-t-lg',
                 'selected' => 'text-primary dark:bg-dark-700 dark:text-dark-300 bg-white font-medium',
                 'unselected' => 'dark:text-dark-200 opacity-50',
             ],
