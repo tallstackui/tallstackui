01070ff1747d1affedae06f1d7b64f221310386f
AJ <alvaro.meireles@live.com>
2023-11-21 21:36:51 -0300

    changing slide lg by default

--- src/config.php
+++ src/config.php
@@ -37,7 +37,7 @@ return [
             'blur' => false,
             'persistent' => false,
             /* Alloweds: sm, md, lg, xl, 2xl, 3xl, 4xl, 5xl, 6xl, 7xl, full */
-            'size' => '2xl',
+            'size' => 'lg',
             /* Alloweds: right, left */
             'position' => 'right',
         ],
