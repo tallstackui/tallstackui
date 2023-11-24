2486d0a4b4028e1388f12fa5c80e37a6bdebfc40
AJ <alvaro.meireles@live.com>
2023-11-24 01:49:57 -0300

    removing full widget concept

--- src/resources/views/components/tab/tab.blade.php
+++ src/resources/views/components/tab/tab.blade.php
@@ -112,28 +112,3 @@
         </div>
     </div>
 </div>
-
-{{-- Full Widget --}}
-<div>
-    <div class="sm:hidden">
-        <label for="tabs" class="sr-only">Select a tab</label>
-        <!-- Use an "onChange" listener to redirect the user to the selected tab URL. -->
-        <select id="tabs" name="tabs" class="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500">
-            <option>My Account</option>
-            <option>Company</option>
-            <option selected>Team Members</option>
-            <option>Billing</option>
-        </select>
-    </div>
-    <div class="hidden sm:block">
-        <div class="border-b border-gray-200">
-            <nav class="-mb-px flex" aria-label="Tabs">
-                <!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
-                <a href="#" class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 w-1/4 border-b-2 py-4 px-1 text-center text-sm font-medium">My Account</a>
-                <a href="#" class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 w-1/4 border-b-2 py-4 px-1 text-center text-sm font-medium">Company</a>
-                <a href="#" class="border-indigo-500 text-indigo-600 w-1/4 border-b-2 py-4 px-1 text-center text-sm font-medium" aria-current="page">Team Members</a>
-                <a href="#" class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 w-1/4 border-b-2 py-4 px-1 text-center text-sm font-medium">Billing</a>
-            </nav>
-        </div>
-    </div>
-</div>
