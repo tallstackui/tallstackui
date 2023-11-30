91069a4ba21b26e2a8ee7d6a9e80c5a870e2775f
AJ <alvaro.meireles@live.com>
2023-11-30 15:01:31 -0300

    fixing tests & positions

--- src/resources/views/components/form/input.blade.php
+++ src/resources/views/components/form/input.blade.php
@@ -13,23 +13,22 @@
             <x-icon :$icon :$error @class([$personalize['icon.size'], 'text-secondary-500' => !$validate]) />
         </div>
     @endif
-    <div
-        {{ $attributes->class([
-           $personalize['input.class.color'] => !$error,
-           $personalize['input.class.wrapper'],
-           $personalize['input.paddings.left'] => $icon && ($position === null || $position === 'left'),
-           $personalize['input.paddings.right'] => $icon && $position === 'right',
-           $personalize['input.class.disabled'] => $attributes->get('disabled') || $attributes->get('readonly'),
-           $personalize['error'] => $error && $validate
-    ]) }}>
-        @if($suffix)
-            <span @class([$personalize['input.class.slot'], $personalize['error'] => $error && $validate])>{{ $suffix }}</span>
+    <div @class([
+            $personalize['input.class.color'] => !$error,
+            $personalize['input.class.wrapper'],
+            $personalize['input.paddings.left'] => $icon && ($position === null || $position === 'left'),
+            $personalize['input.paddings.right'] => $icon && $position === 'right',
+            $personalize['input.class.disabled'] => $attributes->get('disabled') || $attributes->get('readonly'),
+            $personalize['error'] => $error && $validate
+        ])>
+        @if ($prefix)
+            <span @class([$personalize['input.class.slot'], $personalize['error'] => $error && $validate])>{{ $prefix }}</span>
         @endif
         <input id="{{ $id }}" {{ $attributes->class([
             $personalize['input.class.base'],
         ]) }}>
-        @if($prefix)
-            <span @class([$personalize['input.class.slot'], $personalize['error'] => $error && $validate])>{{ $prefix }}</span>
+        @if ($suffix)
+            <span @class([$personalize['input.class.slot'], $personalize['error'] => $error && $validate])>{{ $suffix }}</span>
         @endif
     </div>
 </x-wrapper.input>
