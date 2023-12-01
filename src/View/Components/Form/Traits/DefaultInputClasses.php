69e603927361e6bac0b2fad427201f0d7d2195af
AJ <alvaro.meireles@live.com>
2023-12-01 03:56:32 -0300

    increase suffix and prefix color weight when dark

--- src/View/Components/Form/Traits/DefaultInputClasses.php
+++ src/View/Components/Form/Traits/DefaultInputClasses.php
@@ -22,7 +22,7 @@ trait DefaultInputClasses
         return [
             'wrapper' => 'dark:bg-dark-800 focus:ring-primary-600 focus-within:focus:ring-primary-600 focus-within:ring-primary-600 dark:focus-within:ring-primary-600 flex rounded-md bg-white px-2 shadow-sm ring-1 ring-inset transition focus-within:ring-2 focus-within:ring-inset focus:ring-gray-300 sm:text-sm sm:leading-6',
             'base' => 'w-full border-0 bg-transparent p-1 py-1.5 ring-0 focus:ring-transparent sm:text-sm sm:leading-6',
-            'slot' => 'm-1 flex select-none items-center whitespace-nowrap text-gray-500 transition sm:text-sm',
+            'slot' => 'dark:text-dark-400 m-1 flex select-none items-center whitespace-nowrap text-gray-500 transition sm:text-sm',
             'color' => 'dark:bg-dark-800 dark:ring-dark-600 dark:text-dark-300 dark:placeholder-dark-500 bg-white text-gray-600 ring-gray-300 placeholder:text-gray-400',
             'disabled' => 'bg-gray-50 text-gray-500 dark:bg-gray-600',
         ];
