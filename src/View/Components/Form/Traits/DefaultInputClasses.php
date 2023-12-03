d9e66586139e042d1efdce64028afbf984cb233d
AJ <alvaro.meireles@live.com>
2023-12-03 15:55:36 -0300

    formatting

--- src/View/Components/Form/Traits/DefaultInputClasses.php
+++ src/View/Components/Form/Traits/DefaultInputClasses.php
@@ -20,13 +20,13 @@ trait DefaultInputClasses
     private function input(): array
     {
         return [
-            'wrapper' => 'focus:ring-primary-600 focus-within:focus:ring-primary-600 focus-within:ring-primary-600 dark:focus-within:ring-primary-600 flex rounded-md px-2 shadow-sm ring-1 ring-inset transition focus-within:ring-2 focus-within:ring-inset focus:ring-gray-300',
+            'wrapper' => 'focus:ring-primary-600 focus-within:focus:ring-primary-600 focus-within:ring-primary-600 dark:focus-within:ring-primary-600 flex rounded-md px-2 ring-1 ring-inset transition focus-within:ring-2 focus-within:ring-inset focus:ring-gray-300',
             'base' => 'w-full border-0 bg-transparent p-1 py-1.5 ring-0 focus:ring-transparent sm:text-sm sm:leading-6',
             'slot' => 'dark:text-dark-400 m-1 flex select-none items-center whitespace-nowrap text-gray-500 transition sm:text-sm',
             'color' => [
                 'base' => 'dark:ring-dark-600 dark:text-dark-300 dark:placeholder-dark-500 text-gray-600 ring-gray-300 placeholder:text-gray-400',
-                'background' => 'bg-white dark:bg-dark-800',
-                'disabled' => 'bg-gray-100 dark:bg-gray-600',
+                'background' => 'dark:bg-dark-800 bg-white',
+                'disabled' => 'dark:bg-dark-600 bg-gray-100',
             ],
         ];
     }
--- src/resources/views/components/form/input.blade.php
+++ src/resources/views/components/form/input.blade.php
@@ -2,23 +2,21 @@
     $computed = $attributes->whereStartsWith('wire:model')->first();
     $error = $computed && $errors->has($computed);
     $personalize = tallstackui_personalization('form.input', $personalization());
-    $type = $attributes->get('type');
+    $disabled = $attributes->get('disabled');
+    $readonly = $attributes->get('readonly');
 @endphp
 
 <x-wrapper.input :$id :$computed :$error :$label :$hint :$validate>
     @if ($icon)
-        <div @class([
-                $personalize['icon.wrapper'],
-                $personalize['icon.paddings.' . $position],
-            ])>
+        <div @class([ $personalize['icon.wrapper'], $personalize['icon.paddings.' . $position]])>
             <x-icon :$icon :$error @class([$personalize['icon.size'], 'text-secondary-500' => !$validate]) />
         </div>
     @endif
     <div @class([
             $personalize['input.class.wrapper'],
             $personalize['input.class.color.base'] => !$error,
-            $personalize['input.class.color.background'] => !$attributes->get('disabled') && !$attributes->get('readonly'),
-            $personalize['input.class.color.disabled'] => $attributes->get('disabled') || $attributes->get('readonly'),
+            $personalize['input.class.color.background'] => !$disabled && !$readonly,
+            $personalize['input.class.color.disabled'] => $disabled || $readonly,
             $personalize['input.paddings.left'] => $icon && ($position === null || $position === 'left'),
             $personalize['input.paddings.right'] => $icon && $position === 'right',
             $personalize['error'] => $error && $validate
@@ -26,7 +24,7 @@
         @if ($prefix)
             <span @class([$personalize['input.class.slot'], $personalize['error'] => $error && $validate])>{{ $prefix }}</span>
         @endif
-        <input id="{{ $id }}" @if ($type) type="{{ $type }}" @endif {{ $attributes->class([$personalize['input.class.base']]) }}>
+        <input id="{{ $id }}" {{ $attributes->class($personalize['input.class.base']) }}>
         @if ($suffix)
             <span @class([$personalize['input.class.slot'], $personalize['error'] => $error && $validate])>{{ $suffix }}</span>
         @endif
