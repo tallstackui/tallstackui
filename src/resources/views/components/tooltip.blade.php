9c6db3727140efedd810a0a6b7f4cffbeadb752e
hitaloramon <hitaloramon@live.com>
2023-12-02 21:35:52 -0300

    add animation, anchor and removed arrow

--- src/resources/views/components/tooltip.blade.php
+++ src/resources/views/components/tooltip.blade.php
@@ -1,42 +1,26 @@
-@php($personalize = tallstackui_personalization('tooltip', $personalization()))
+@php
+    $personalize = tallstackui_personalization('tooltip', $personalization());
+    $side = str_contains($position, 'right') || str_contains($position, 'left');
+    $orientation = str_contains($position, 'bottom') || str_contains($position, 'right');
+@endphp
 
 <div class="inline-flex" x-data>
     <div class="relative" x-data="{ show : false }">
         <div x-ref="tooltip"
              x-show="show"
+             x-transition:enter="transition duration-100 ease-out"
+             x-transition:enter-start="opacity-0 @if ($side) @if ($orientation) -translate-x-2 @else translate-x-2 @endif @else @if ($orientation) -translate-y-2 @else translate-y-2 @endif @endif"
+             x-transition:enter-end="opacity-100 @if ($side) translate-x-0 @else translate-y-0 @endif"
+             x-anchor.{{ $position }}.offset.10="$refs.content"
+             x-bind:style="{ position: 'absolute', top: $anchor.y+'px', left: $anchor.x+'px' }"
              x-cloak
-             @class([
-                'absolute min-w-max',
-                'top-0 left-1/2 -translate-x-1/2 -mt-1.5 -translate-y-full' => $position === 'top',
-                'top-1/2 -translate-y-1/2 -ml-1.5 left-0 -translate-x-full' => $position === 'left',
-                'bottom-0 left-1/2 -translate-x-1/2 -mb-1.5 translate-y-full' => $position === 'bottom',
-                'top-1/2 -translate-y-1/2 -mr-1.5 right-0 translate-x-full' => $position === 'right',
-             ])>
+             class="absolute z-10 w-max">
             <div x-show="show" @class([
                      $personalize['color.default'] => $configurations['thematic'] === false,
                      $personalize['color.thematic'] => $configurations['thematic'] === true,
                      $personalize['wrapper']
                  ])>
-                <p class="text-sm">{!! $text ?? $slot !!}</p>
-                <div x-ref="arrow"
-                    @class([
-                        'absolute inline-flex items-center justify-center',
-                        'bottom-0 -translate-x-1/2 left-1/2 w-5 translate-y-full' => $position === 'top',
-                        'right-0 -translate-y-1/2 top-1/2 h-5 translate-x-full' => $position === 'left',
-                        'top-0 -translate-x-1/2 left-1/2 w-5 -translate-y-full' => $position === 'bottom',
-                        'left-0 -translate-y-1/2 top-1/2 h-5 -translate-x-full' => $position === 'right',
-                    ])>
-                    <div @class([
-                        $personalize['color.default'] => $configurations['thematic'] === false,
-                        $personalize['color.thematic'] => $configurations['thematic'] === true,
-                        'w-1.5 h-1.5 transform',
-                        'origin-top-left -rotate-45' => $position === 'top',
-                        'origin-top-left rotate-45' => $position === 'left',
-                        'origin-bottom-left rotate-45' => $position === 'bottom',
-                        'origin-top-right -rotate-45' => $position === 'right',
-                    ])>
-                    </div>
-                </div>
+                <p class="text-sm relative z-20">{!! $text ?? $slot !!}</p>
             </div>
         </div>
         <div x-ref="content"
