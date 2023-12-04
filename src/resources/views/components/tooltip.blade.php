3ae1fa05b2f6af5d6b7bf8d3eede1d7284100acb
AJ <alvaro.meireles@live.com>
2023-12-03 22:11:27 -0300

    wip

--- src/resources/views/components/tooltip.blade.php
+++ src/resources/views/components/tooltip.blade.php
@@ -1,9 +1,12 @@
-@php($personalize = tallstackui_personalization('tooltip', $personalization()))
+@php
+    $personalize = tallstackui_personalization('tooltip', $personalization());
+    $text ??= $slot->toHtml();
+@endphp
 
 <div @class($personalize['wrapper']) x-data>
     <x-dynamic-component component="tallstack-ui::icon.{{ $style }}.{{ $icon }}"
                          data-position="{{ $position }}"
-                         x-tooltip="{!! $text ?? $slot->toHtml() !!}"
+                         x-tooltip="{!! $text !!}"
                         {{ $attributes->class([$personalize['sizes.' . $size], $colors['icon']]) }}
     />
 </div>
