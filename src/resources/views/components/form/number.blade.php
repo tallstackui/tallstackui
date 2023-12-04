aafd16704b2d6054055a691eb62c33644e626ee1
AJ <60591772+devajmeireles@users.noreply.github.com>
2023-12-04 17:40:35 -0300

    Update src/resources/views/components/form/number.blade.php

--- src/resources/views/components/form/number.blade.php
+++ src/resources/views/components/form/number.blade.php
@@ -37,7 +37,7 @@
                     x-on:touchend="clearInterval(interval);"
                     x-bind:class="{ 'opacity-30': atMin, 'pointer-events-none opacity-50': disabled }"
                     type="button"
-                    @if($disabled || $readonly) disabled @endif
+                    @if ($disabled || $readonly) disabled @endif
                     dusk="tallstackui_form_number_decrement"
                     @class($personalize['buttons.left.base'])>
                 <x-icon :name="$icons['left']"
