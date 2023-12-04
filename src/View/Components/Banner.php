8f16dbd642ec1340ca8b1012b8bb3d094ee17d63
hitaloramon <hitaloramon@live.com>
2023-12-04 19:07:28 -0300

    Add icon and sticky position

--- src/View/Components/Banner.php
+++ src/View/Components/Banner.php
@@ -52,6 +52,7 @@ class Banner extends Component implements Personalization
             ],
             'slot.left' => 'absolute left-0 ml-4 text-sm font-medium',
             'text' => 'flex-grow text-center text-sm font-medium',
+            'icon' => 'w-5 h-5 text-white',
             'close' => 'h-4 w-4 cursor-pointer',
         ]);
     }
