14c0032cb2b892525c569954198cadd70022a137
Tanju Yildiz <ben@tanjuyildiz.com>
2023-12-05 12:35:42 +0300

    Add Turkish language translations

---
+++ src/contents/lang/tr/messages.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'errors' => [
+        'title' => ':count doğrulama hatası mevcut:',
+    ],
+
+    'select' => [
+        'default' => 'Seçim yapınız',
+        'search' => 'Arama yapın...',
+        'empty' => 'Sonuç bulunamadı',
+    ],
+
+    'toast' => [
+        'button' => [
+            'ok' => 'Tamam',
+            'confirm' => 'Onayla',
+            'cancel' => 'Vazgeç',
+        ],
+    ],
+
+    'dialog' => [
+        'button' => [
+            'ok' => 'Tamam',
+            'confirm' => 'Onayla',
+            'cancel' => 'Vazgeç',
+        ],
+    ],
+];
