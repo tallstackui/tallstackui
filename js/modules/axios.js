972c734772c3a30656c0853f6142e4d6d2979634
AJ <alvaro.meireles@live.com>
2023-12-08 20:51:35 -0300

    refactoring js folder structure

--- js/modules/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from 'js/modules/axios';
-
-axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-axios.defaults.headers.common['Accept'] = 'application/json';
-
-export default axios;
