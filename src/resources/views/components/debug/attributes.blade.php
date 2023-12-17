04c4aa001ae81d29a589d17fa62217b7288237d3
AJ <alvaro.meireles@live.com>
2023-12-17 00:52:35 -0300

    add support for extra attributes

--- src/resources/views/components/debug/attributes.blade.php
+++ src/resources/views/components/debug/attributes.blade.php
@@ -1,21 +1,22 @@
 @php
-    $component = $data['componentName'];
+    $name = $data['componentName'];
+    $attributes = $data['attributes'];
     $ignores = ['slot', 'trigger', 'content', 'componentName'];
 
     // Although we can use a single filter here, it was
     // preferable to do it this way to increase readability.
-    $attributes = collect($data)
+    $properties = collect($data)
         ->filter(fn (mixed $value, string $key) => ! is_array($value))
         ->filter(fn (mixed $value, string $key) => ! is_callable($value))
-        ->filter(fn (mixed $value, string $key) => ! in_array($key, $ignores))
+        ->filter(fn (mixed $value, string $key) => ! in_array($key, $ignores));
 @endphp
 
 <div>
     <span class="py flex justify-center rounded-lg bg-red-500 px-1">
-        {{ $component }}
+        {{ $name }}
     </span>
     <ul class="mt-2">
-        @forelse ($attributes as $key => $value)
+        @forelse ($properties as $key => $value)
             <li>{{ $key }}: <span class="text-red-500">{{ $value }}</span></li>
             @if ($loop->last && $data['slot']->isNotEmpty())
                 <li class="inline-flex gap-x-1">slot mode: <x-tallstack-ui::icon.solid.check class="w-4 h-4 text-green-500" /></li>
@@ -24,5 +25,15 @@
             <span class="text-white">No attributes</span>
         @endforelse
     </ul>
+    @if ((string) $attributes !== '')
+        <span class="mt-2 py flex justify-center text-red-500 px-1">
+            Attributes
+        </span>
+        <ul>
+            @foreach ($attributes as $key => $value)
+                <li>{{ $key }}: <span class="text-red-500">{{ $value }}</span></li>
+            @endforeach
+        </ul>
+    @endif
 </div>
 
