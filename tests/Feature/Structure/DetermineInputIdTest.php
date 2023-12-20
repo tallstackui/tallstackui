91c4fcaf67f9f7e8a060e2034a228cfe53821f6a
AJ <alvaro.meireles@live.com>
2023-12-20 01:08:28 -0300

    writting tests

---
+++ tests/Feature/Structure/DetermineInputIdTest.php
@@ -0,0 +1,58 @@
+<?php
+
+use TallStackUi\View\Components\Form\Checkbox;
+use TallStackUi\View\Components\Form\Color;
+use TallStackUi\View\Components\Form\Hint;
+use TallStackUi\View\Components\Form\Input;
+use TallStackUi\View\Components\Form\Label;
+use TallStackUi\View\Components\Form\Password;
+use TallStackUi\View\Components\Form\Pin;
+use TallStackUi\View\Components\Form\Radio;
+use TallStackUi\View\Components\Form\Range;
+use TallStackUi\View\Components\Form\Textarea;
+use TallStackUi\View\Components\Form\Toggle;
+use TallStackUi\View\Components\Form\Traits\DetermineInputId;
+use TallStackUi\View\Components\Wrapper\Input as InputWrapper;
+use TallStackUi\View\Components\Wrapper\Radio as RadioWrapper;
+
+describe('DetermineInputId', function () {
+    test('should use the trait')
+        ->expect([
+            Input::class,
+            Password::class,
+            Textarea::class,
+            Radio::class,
+            Range::class,
+            Checkbox::class,
+            Toggle::class,
+            Color::class,
+        ])->toUse(DetermineInputId::class);
+
+    test('should not use the trait')
+        ->expect([
+            Pin::class,
+            Error::class,
+            Label::class,
+            Hint::class,
+            InputWrapper::class,
+            RadioWrapper::class,
+        ])->not->toUse(DetermineInputId::class);
+
+    it('should return the correct id based on id', function () {
+        $component = <<<'HTML'
+        <x-input id="foo-bar-baz" />
+        HTML;
+
+        $this->blade($component)
+            ->assertSee('id="foo-bar-baz"', false);
+    });
+
+    it('should return the correct id based on name', function () {
+        $component = <<<'HTML'
+        <x-input name="foo-bar-baz" />
+        HTML;
+
+        $this->blade($component)
+            ->assertSee('id="foo-bar-baz"', false);
+    });
+});
