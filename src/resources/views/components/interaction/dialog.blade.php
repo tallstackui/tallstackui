bac325927d0cdd260360e6e0f56dd92a7e58d489
AJ <alvaro.meireles@live.com>
2023-12-21 01:43:06 -0300

    wip

--- src/resources/views/components/interaction/dialog.blade.php
+++ src/resources/views/components/interaction/dialog.blade.php
@@ -49,22 +49,22 @@
                         <div x-show="dialog.type === 'error'">
                             <x-icon name="x-circle"
                                     outline
-                                    @class([$personalize['icon.size'], $colors['icon']['success']]) />
+                                    @class([$personalize['icon.size'], $colors['icon']['error']]) />
                         </div>
                         <div x-show="dialog.type === 'info'">
                             <x-icon name="information-circle"
                                     outline
-                                    @class([$personalize['icon.size'], $colors['icon']['success']]) />
+                                    @class([$personalize['icon.size'], $colors['icon']['info']]) />
                         </div>
                         <div x-show="dialog.type === 'warning'">
                             <x-icon name="exclamation-circle"
                                     outline
-                                    @class([$personalize['icon.size'], $colors['icon']['success']]) />
+                                    @class([$personalize['icon.size'], $colors['icon']['warning']]) />
                         </div>
                         <div x-show="dialog.type === 'question'">
                             <x-icon name="question-mark-circle"
                                     outline
-                                    @class([$personalize['icon.size'], $colors['icon']['success']]) />
+                                    @class([$personalize['icon.size'], $colors['icon']['question']]) />
                         </div>
                     </div>
                     <div @class($personalize['text.wrapper'])>
@@ -86,11 +86,11 @@
                     <button @class($personalize['buttons.confirm']) x-bind:class="{
                             'sm:w-auto' : dialog.type === 'question',
                             'col-span-full' : dialog.type !== 'question',
-                            '{{ $colors['confirm']['success'] }}': dialog.type === 'success',
-                            '{{ $colors['confirm']['error'] }}': dialog.type === 'error',
-                            '{{ $colors['confirm']['info'] }}': dialog.type === 'info',
-                            '{{ $colors['confirm']['warning'] }}': dialog.type === 'warning',
-                            '{{ $colors['confirm']['question'] }}': dialog.type === 'question'
+                            'bg-green-600 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-green-100 dark:ring-offset-green-900' : dialog.type === 'success',
+                            'bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-100 dark:ring-offset-red-900' : dialog.type === 'error',
+                            'bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-100 dark:ring-offset-blue-900' : dialog.type === 'info',
+                            'bg-yellow-600 hover:bg-yellow-700 focus:ring-yellow-500 focus:ring-offset-yellow-100 dark:ring-offset-yellow-900' : dialog.type === 'warning',
+                            'bg-primary-600 hover:bg-primary-700 focus:ring-primary-500 focus:ring-offset-primary-100 dark:ring-offset-primary-900' : dialog.type === 'question'
                         }" dusk="tallstackui_dialog_confirmation"
                            x-on:click="accept(dialog)"
                            x-text="dialog.type === 'question' ? dialog.options.confirm.text : text.ok"></button>
