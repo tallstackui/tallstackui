2ea403a36be99ec83e048fc6f726390a9dd65dd6
AJ <alvaro.meireles@live.com>
2023-12-26 22:35:43 -0300

    formatting tailwindcss classes

--- src/resources/views/components/table/index.blade.php
+++ src/resources/views/components/table/index.blade.php
@@ -24,18 +24,18 @@
             @endif
         </div>
     @endif
-    <div class="overflow-auto rounded-lg shadow ring-1 ring-gray-300 dark:ring-dark-600 soft-scrollbar">
-        <table class="min-w-full divide-y divide-gray-200 dark:divide-dark-500/50" @if ($loading) wire:loading.class="opacity-25 cursor-not-allowed select-none" @endif>
+    <div class="soft-scrollbar dark:ring-dark-600 overflow-auto rounded-lg shadow ring-1 ring-gray-300">
+        <table class="dark:divide-dark-500/50 min-w-full divide-y divide-gray-200" @if ($loading) wire:loading.class="cursor-not-allowed select-none opacity-25" @endif>
             @if ($loading)
-                <x-tallstack-ui::icon.others.loading class="absolute top-1/2 left-1/2 w-10 h-10 text-primary-500 dark:text-dark-300 animate-spin"
+                <x-tallstack-ui::icon.others.loading class="text-primary-500 dark:text-dark-300 absolute left-1/2 top-1/2 h-10 w-10 animate-spin"
                                                      wire:loading="{{ $target }}" />
             @endif
             @if (!$headerless)
                 <thead @class(['uppercase', 'bg-gray-50 dark:bg-dark-600' => !$striped, 'bg-white dark:bg-dark-700' => $striped])>
                 <tr>
                     @foreach ($headers as $header)
-                        <th scope="col" class="py-3.5 text-left text-sm font-semibold text-gray-700 dark:text-dark-200 px-3">
-                            <a class="group inline-flex cursor-pointer truncate"
+                        <th scope="col" class="dark:text-dark-200 px-3 py-3.5 text-left text-sm font-semibold text-gray-700">
+                            <a class="inline-flex cursor-pointer truncate"
                                @if ($livewire && $sortable($header))
                                     wire:click="$set('sort', {column: '{{ $head($header)['column'] }}', direction: '{{ $head($header)['direction'] }}' })"
                                 @endif>
@@ -43,7 +43,7 @@
                                 {{ $header['label'] ?? '' }}
 
                                 @if ($livewire && $sortable($header) && $sorted($header))
-                                    <x-icon :name="$head($header)['direction'] === 'desc' ? 'chevron-up' : 'chevron-down'"  class="w-4 h-4 ml-2" />
+                                    <x-icon :name="$head($header)['direction'] === 'desc' ? 'chevron-up' : 'chevron-down'"  class="ml-2 h-4 w-4" />
                                 @endif
                             </a>
                         </th>
@@ -51,17 +51,17 @@
                 </tr>
                 </thead>
             @endif
-            <tbody class="divide-y divide-gray-200 bg-white dark:bg-dark-700 dark:divide-dark-500/20">
+            <tbody class="dark:bg-dark-700 dark:divide-dark-500/20 divide-y divide-gray-200 bg-white">
             @forelse ($rows as $key => $value)
                 <tr @class(['bg-gray-50 dark:bg-dark-800/50' => $striped && $loop->index % 2 === 0])>
                     @foreach($headers as $header)
                         @php($row = str_replace('.', '_', $header['index']))
                         @if (isset(${$row}))
-                            <td class="whitespace-nowrap py-4 text-sm px-3 text-gray-500 dark:text-dark-300">
+                            <td class="dark:text-dark-300 whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                                 {{ ${$row}($value) }}
                             </td>
                         @else
-                            <td class="whitespace-nowrap py-4 text-sm px-3 text-gray-500 dark:text-dark-300">
+                            <td class="dark:text-dark-300 whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                                 {{ data_get($value, $header['index']) }}
                             </td>
                         @endif
@@ -69,7 +69,7 @@
                 </tr>
             @empty
                 <tr>
-                    <td class="whitespace-nowrap py-4 text-sm px-3 text-gray-500 dark:text-dark-300 col-span-full" colspan="100%">
+                    <td class="dark:text-dark-300 col-span-full whitespace-nowrap px-3 py-4 text-sm text-gray-500" colspan="100%">
                         {{ $placeholders['empty'] }}
                     </td>
                 </tr>
