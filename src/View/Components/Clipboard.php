cf4f8a4df67add5b508e27929f23aca862791308
AJ <alvaro.meireles@live.com>
2023-12-29 12:06:22 -0300

    introducing classes as blocks

--- src/View/Components/Clipboard.php
+++ src/View/Components/Clipboard.php
@@ -5,15 +5,18 @@ namespace TallStackUi\View\Components;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Arr;
 use TallStackUi\Foundation\Attributes\SkipDebug;
+use TallStackUi\Foundation\Attributes\SoftPersonalization;
 use TallStackUi\Foundation\Personalization\Contracts\Personalization;
+use TallStackUi\View\Components\Form\Traits\DefaultInputClasses;
 
-// TODO: different icons when icon
+#[SoftPersonalization('clipboard')]
 class Clipboard extends BaseComponent implements Personalization
 {
+    use DefaultInputClasses;
+
     public function __construct(
         public ?string $label = null,
         public ?string $text = null,
-        public ?bool $textarea = null,
         public ?bool $icon = null,
         public ?bool $left = false,
         #[SkipDebug]
@@ -24,7 +27,7 @@ class Clipboard extends BaseComponent implements Personalization
         // TODO: validate?
         $this->placeholders = __('tallstack-ui::messages.clipboard');
 
-        $this->type = ! $this->textarea && ! $this->icon ? 'input' : ($this->textarea ? 'textarea' : 'icon');
+        $this->type = ! $this->icon ? 'input' : 'icon';
     }
 
     public function blade(): View
@@ -34,6 +37,36 @@ class Clipboard extends BaseComponent implements Personalization
 
     public function personalization(): array
     {
-        return Arr::dot([]);
+        return Arr::dot([
+            'input' => [
+                'wrapper' => 'relative flex flex-grow items-stretch focus-within:z-10',
+                'buttons' => [
+                    'base' => 'relative inline-flex items-center gap-x-1.5 px-3 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 dark:ring-dark-600 dark:text-dark-300 transition',
+                    'left' => 'rounded-l-md -mr-px',
+                    'right' => 'rounded-r-md -ml-px',
+                    'icon' => [
+                        'name' => 'clipboard-document',
+                        'class' => 'h-5 w-5 text-primary-500 cursor-pointer',
+                    ],
+                ],
+                'class' => [
+                    'base' => 'focus:ring-primary-600 dark:focus:ring-primary-600 block w-full rounded-none border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 transition placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6',
+                    'color' => [...$this->input()['color']],
+                ],
+            ],
+            'icon' => [
+                'wrapper' => 'inline-flex',
+                'icons' => [
+                    'copy' => [
+                        'name' => 'clipboard',
+                        'class' => 'h-5 w-5 text-primary-500 cursor-pointer',
+                    ],
+                    'copied' => [
+                        'name' => 'document-check',
+                        'class' => 'h-5 w-5 text-green-500 cursor-none',
+                    ],
+                ],
+            ],
+        ]);
     }
 }
--- src/resources/views/components/clipboard.blade.php
+++ src/resources/views/components/clipboard.blade.php
@@ -1,6 +1,7 @@
 @php
     $text ??= $slot->toHtml();
     $hash = md5($text.uniqid());
+    $personalize = $classes();
 @endphp
 
 <div x-data="tallstackui_clipboard(@js($text), @js($hash), @js($type), @js($placeholders['button']))">
@@ -12,16 +13,18 @@
             @if ($left)
                 <button data-hash="{{ $hash }}"
                         x-on:click="copy()"
-                        class="'relative -mr-px inline-flex items-center gap-x-1.5 rounded-l-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50'"
+                        @class([$personalize['input.buttons.base'], $personalize['input.buttons.left.base']])
                         type="button">
-                    <x-icon name="clipboard-document" class="h-5 w-5 text-primary-500 cursor-pointer" />
+                    <x-icon :name="$personalize['input.buttons.icon.name']" @class($personalize['input.buttons.icon.class']) />
                     <p x-ref="input-{{ $hash }}">{{ $placeholders['button']['copy'] }}</p>
                 </button>
             @endif
-            <div class="relative flex flex-grow items-stretch focus-within:z-10">
+            <div @class($personalize['input.wrapper'])>
                 <input type="text"
                        @class([
-                            'block w-full rounded-none border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+                            $personalize['input.class.color.base'],
+                            $personalize['input.class.color.background'],
+                            $personalize['input.class.base'],
                             'rounded-l-md' => ! $left,
                             'rounded-r-md' => $left,
                        ]) value="{{ $text }}" readonly>
@@ -29,42 +32,22 @@
             @if (! $left)
                 <button data-hash="{{ $hash }}"
                         x-on:click="copy()"
-                        type="button"
-                        class="relative -ml-px inline-flex items-center rounded-r-md gap-x-1.5 px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                    <x-icon name="clipboard-document" class="h-5 w-5 text-primary-500 cursor-pointer" />
+                        @class([$personalize['input.buttons.base'], $personalize['input.buttons.right']])
+                        type="button">
+                    <x-icon :name="$personalize['input.buttons.icon.name']" @class($personalize['input.buttons.icon.class']) />
                     <p x-ref="input-{{ $hash }}">{{ $placeholders['button']['copy'] }}</p>
                 </button>
             @endif
         @endif
-        @if ($type === 'textarea')
-            <div class="relative min-w-0 flex-1">
-                <div class="overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
-                    <textarea rows="3" name="comment" id="comment" class="block w-full resize-none border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">{{ $text }}</textarea>
-                    <div class="py-2" aria-hidden="true">
-                        <div class="py-px">
-                            <div class="h-9"></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="absolute inset-x-0 bottom-0 flex justify-end py-2 pl-3 pr-2">
-                    <div class="inline-flex">
-                        <x-button data-hash="{{ $hash }}" x-on:click.prevent="copy()" sm>
-                            <x-icon name="clipboard-document"  class="h-5 w-5 text-white cursor-pointer" />
-                            <p x-ref="textarea-{{ $hash }}">{{ $placeholders['button']['copy'] }}</p>
-                        </x-button>
-                    </div>
-                </div>
-            </div>
-        @endif
         @if ($type === 'icon')
-            <div class="inline-flex">
-                <x-icon name="clipboard"
-                        class="h-5 w-5 text-primary-500 cursor-pointer"
+            <div @class($personalize['icon.wrapper'])>
+                <x-icon :name="$personalize['icon.icons.copy.name']"
                         data-hash="{{ $hash }}"
-                        x-on:click="copy()" x-show="!notification" />
-                <x-icon name="document-check"
-                        class="h-5 w-5 cursor-none"
-                        x-bind:class="{ 'text-green-500': notification, 'text-red-500': !notification }"
+                        @class($personalize['icon.icons.copy.class'])
+                        x-on:click="copy()"
+                        x-show="!notification" />
+                <x-icon :name="$personalize['icon.icons.copied.name']"
+                        @class($personalize['icon.icons.copied.class'])
                         x-show="notification" />
             </div>
         @endif
