541b9c745e6b5f21d5e1251790695b1a823fe8b7
AJ <alvaro.meireles@live.com>
2023-12-29 10:57:57 -0300

    using a single data provider

--- src/resources/views/components/clipboard.blade.php
+++ src/resources/views/components/clipboard.blade.php
@@ -3,43 +3,41 @@
     $hash = md5($text.uniqid());
 @endphp
 
-<div>
+<div x-data="tallstackui_clipboard(@js($text), @js($hash), @js($type), @js($placeholders['button']))">
     @if ($label)
         <x-label :$label/>
     @endif
     <div @class(['mt-1', 'flex' => $type === 'input'])>
         @if ($type === 'input')
-            <div class="w-full flex" x-data="tallstackui_clipboard(@js($text), @js($hash), @js($type), @js($placeholders['button']))">
-                @if ($left)
-                    <button data-hash="{{ $hash }}"
-                            x-on:click="copy()"
-                            class="'relative -mr-px inline-flex items-center gap-x-1.5 rounded-l-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50'"
-                            type="button">
-                        <x-icon name="clipboard-document" class="h-5 w-5 text-primary-500 cursor-pointer" />
-                        <p x-ref="input-{{ $hash }}">{{ $placeholders['button']['copy'] }}</p>
-                    </button>
-                @endif
-                <div class="relative flex flex-grow items-stretch focus-within:z-10">
-                    <input type="text"
-                           @class([
-                                'block w-full rounded-none border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
-                                'rounded-l-md' => ! $left,
-                                'rounded-r-md' => $left,
-                           ]) value="{{ $text }}" readonly>
-                </div>
-                @if (! $left)
-                    <button data-hash="{{ $hash }}"
-                            x-on:click="copy()"
-                            type="button"
-                            class="relative -ml-px inline-flex items-center rounded-r-md gap-x-1.5 px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                        <x-icon name="clipboard-document" class="h-5 w-5 text-primary-500 cursor-pointer" />
-                        <p x-ref="input-{{ $hash }}">{{ $placeholders['button']['copy'] }}</p>
-                    </button>
-                @endif
+            @if ($left)
+                <button data-hash="{{ $hash }}"
+                        x-on:click="copy()"
+                        class="'relative -mr-px inline-flex items-center gap-x-1.5 rounded-l-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50'"
+                        type="button">
+                    <x-icon name="clipboard-document" class="h-5 w-5 text-primary-500 cursor-pointer" />
+                    <p x-ref="input-{{ $hash }}">{{ $placeholders['button']['copy'] }}</p>
+                </button>
+            @endif
+            <div class="relative flex flex-grow items-stretch focus-within:z-10">
+                <input type="text"
+                       @class([
+                            'block w-full rounded-none border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+                            'rounded-l-md' => ! $left,
+                            'rounded-r-md' => $left,
+                       ]) value="{{ $text }}" readonly>
             </div>
+            @if (! $left)
+                <button data-hash="{{ $hash }}"
+                        x-on:click="copy()"
+                        type="button"
+                        class="relative -ml-px inline-flex items-center rounded-r-md gap-x-1.5 px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                    <x-icon name="clipboard-document" class="h-5 w-5 text-primary-500 cursor-pointer" />
+                    <p x-ref="input-{{ $hash }}">{{ $placeholders['button']['copy'] }}</p>
+                </button>
+            @endif
         @endif
         @if ($type === 'textarea')
-            <div x-data="tallstackui_clipboard(@js($text), @js($hash), @js($type), @js($placeholders['button']))" class="relative min-w-0 flex-1">
+            <div class="relative min-w-0 flex-1">
                 <div class="overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
                     <textarea rows="3" name="comment" id="comment" class="block w-full resize-none border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">{{ $text }}</textarea>
                     <div class="py-2" aria-hidden="true">
@@ -59,7 +57,7 @@
             </div>
         @endif
         @if ($type === 'icon')
-            <div x-data="tallstackui_clipboard(@js($text), @js($hash), @js($type), @js($placeholders['button']))" class="inline-flex">
+            <div class="inline-flex">
                 <x-icon name="clipboard"
                         class="h-5 w-5 text-primary-500 cursor-pointer"
                         data-hash="{{ $hash }}"
