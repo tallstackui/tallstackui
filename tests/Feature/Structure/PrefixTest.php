99dbb2d7ee4d1ffd56bca192b87bbd2f0fe00c33
AJ <alvaro.meireles@live.com>
2024-01-02 00:11:02 -0300

    simple tests for prefixes

---
+++ tests/Feature/Structure/PrefixTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use TallStackUi\Facades\TallStackUi;
+
+describe('Prefixes', function () {
+    test('can set prefix', function () {
+        config()->set('tallstackui.prefix', 'ts-');
+
+        expect(TallStackUi::component('alert'))->toBe('ts-alert');
+    });
+
+    test('can get name without prefix prefix', function () {
+        expect(TallStackUi::component('alert'))->toBe('alert');
+    });
+});
