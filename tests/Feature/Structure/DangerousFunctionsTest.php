e8c9ef5daf6e12ce280aee21e01f1dbfb17c8673
AJ <alvaro.meireles@live.com>
2024-01-03 20:59:58 -0300

    introducing test to don't allow usage of @dd and @dump in Blade files

--- tests/Feature/Structure/DangerousFunctionsTest.php
+++ tests/Feature/Structure/DangerousFunctionsTest.php
@@ -1,6 +1,22 @@
 <?php
 
-test('should not use dangerous functions')
+use Illuminate\Support\Facades\File;
+
+test('should not use dangerous functions in PHP files')
     ->expect(['dd', 'dump', 'exit', 'var_dump'])
     ->not
     ->toBeUsed();
+
+test('should not use dangerous functions in Blade files', function () {
+    $files = collect(File::allFiles(__DIR__.'/../../../src/resources/views/'))
+        ->map(fn (SplFileInfo $file) => ['name' => $file->getFilename(), 'content' => file_get_contents($file->getRealPath())])
+        ->filter(fn (array $file) => str($file['content'])->contains(['@dd', '@dump']))
+        ->pluck('name')
+        ->implode(', ');
+
+    if (! empty($files)) {
+        test()->fail("The following files contain dangerous functions: [{$files}]"); // @phpstan-ignore-line
+    }
+
+    expect($files)->toBeEmpty();
+});
