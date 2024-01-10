02129a8a5bb23225c499c8a6a01cd383d905ef8f
hitaloramon <hitaloramon@live.com>
2024-01-09 21:35:57 -0300

    format

--- src/View/Components/Select/Styled.php
+++ src/View/Components/Select/Styled.php
@@ -93,7 +93,7 @@ class Styled extends BaseComponent implements Personalization
                         'wrapper' => 'dark:text-dark-300 dark:hover:bg-dark-500 dark:focus:bg-dark-500 relative cursor-pointer select-none px-2 py-2 text-gray-700 transition hover:bg-gray-100 focus:bg-gray-100 focus:outline-none',
                         'options' => 'flex items-center justify-between',
                         'selected' => 'font-semibold hover:bg-red-500 hover:text-white dark:hover:bg-red-500',
-                        'disabled' => 'bg-gray-100 dark:bg-dark-500  !cursor-not-allowed',
+                        'disabled' => 'dark:bg-dark-500 !cursor-not-allowed bg-gray-100',
                         'img' => 'h-6 w-6 rounded-full',
                         'check' => 'h-5 w-5',
                     ],
