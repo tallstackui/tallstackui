1f10946c66e94d94b735095059f58cfee3cf30f3
AJ <alvaro.meireles@live.com>
2024-01-09 22:03:37 -0300

    wip

--- src/View/Components/Form/Tag.php
+++ src/View/Components/Form/Tag.php
@@ -29,6 +29,23 @@ class Tag extends BaseComponent implements Personalization
 
     public function personalization(): array
     {
-        return Arr::dot([]);
+        return Arr::dot([
+            'wrapper' => 'flex flex-wrap gap-x-2 border-0 bg-white pr-4 pb-1.5 dark:bg-dark-800',
+            'label' => [
+                'base' => 'inline-flex h-8 items-center rounded-lg bg-gray-100 px-2 text-sm font-medium text-gray-600 ring-1 ring-inset ring-gray-200 mt-1.5 py space-x-1 dark:text-dark-100 dark:bg-dark-700 dark:ring-dark-600',
+                'icon' => 'h-4 w-4 cursor-pointer text-red-500',
+            ],
+            'input' => [
+                'class' => [
+                    'base' => 'flex flex-grow items-center border-0 border-transparent pb-1 text-gray-600 outline-none pt focus:outline-none focus:ring-0',
+                    ...collect($this->input())->except('base')->toArray(),
+                ],
+            ],
+            'icon' => [
+                'wrapper' => 'absolute inset-y-0 right-2 flex items-center text-secondary-500 dark:text-dark-400',
+                'base' => 'h-5 w-5 cursor-pointer text-red-500',
+            ],
+            'error' => $this->error(),
+        ]);
     }
 }
--- src/resources/views/components/form/tag.blade.php
+++ src/resources/views/components/form/tag.blade.php
@@ -7,28 +7,39 @@
     <div x-data="tallstackui_formTag({!! $entangle !!})"
          x-cloak
          x-on:click="$refs.input.focus()"
-         class="relative flex w-full rounded-md px-2 placeholder:text-gray-400 text-gray-600 ring-1 ring-gray-300 transition focus-within:ring-primary-600 focus-within:ring-2 focus:ring-primary-600 dark:ring-dark-600 dark:text-dark-300 dark:placeholder-dark-500 focus-within:focus:ring-primary-600 dark:focus-within:ring-primary-600">
-        <div class="flex flex-wrap gap-x-2 border-0 bg-white pr-4 pb-1.5 dark:bg-dark-800">
+         @class([
+            'relative',
+            $personalize['input.class.wrapper'],
+            $personalize['input.class.color.base'] => !$error,
+            $personalize['input.class.color.background'] => !$attributes->get('disabled') && !$attributes->get('readonly'),
+            $personalize['input.class.color.disabled'] => $attributes->get('disabled') || $attributes->get('readonly'),
+            $personalize['error'] => $error
+         ])>
+        <div @class($personalize['wrapper'])>
             <template x-for="(tag, index) in model" :key="index">
-                <span class="inline-flex h-8 items-center rounded-lg bg-gray-100 px-2 text-sm font-medium text-gray-600 ring-1 ring-inset ring-gray-200 mt-1.5 py space-x-1 dark:text-dark-100 dark:bg-dark-700 dark:ring-dark-600">
+                <span @class($personalize['label.base'])>
                     <span x-text="tag"></span>
                     <x-dynamic-component :component="TallStackUi::component('icon')"
                                          name="x-mark"
-                                         class="h-4 w-4 cursor-pointer text-red-500"
+                                         :class="$personalize['label.icon']"
                                          x-on:click="remove(index)" />
                 </span>
             </template>
-            <input class="flex flex-grow items-center border-0 border-transparent bg-white pb-1 text-gray-600 outline-none pt focus:outline-none focus:ring-0 dark:bg-dark-800 dark:text-dark-300 dark:placeholder-dark-500"
-                   {{ $attributes->whereDoesntStartWith('wire:model') }}
+            <input {{ $attributes->whereDoesntStartWith('wire:model')
+                        ->class([
+                            $personalize['input.class.base'],
+                            $personalize['input.class.color.background'],
+                            $personalize['error'] => $error
+                        ]) }}
                    x-on:keydown="add($event)"
                    x-on:keydown.backspace="remove(model.length - 1)"
                    x-model="tag"
                    x-ref="input">
         </div>
-        <div x-show="model.length > 0" class="absolute inset-y-0 right-2 flex items-center text-secondary-500 dark:text-dark-400" >
+        <div x-show="model.length > 0" @class($personalize['icon.wrapper'])>
             <x-dynamic-component :component="TallStackUi::component('icon')"
                                  name="x-mark"
-                                 class="h-5 w-5 cursor-pointer text-red-500"
+                                 :class="$personalize['icon.base']"
                                  x-on:click="erase()" />
         </div>
     </div>
