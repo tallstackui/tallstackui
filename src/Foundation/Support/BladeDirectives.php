5eaeec52c6ecb829f879a7af6b5c1b4a32841fbd
AJ <60591772+devajmeireles@users.noreply.github.com>
2024-01-12 21:51:09 -0300

    transforming asset router

--- src/Foundation/Support/BladeDirectives.php
+++ src/Foundation/Support/BladeDirectives.php
@@ -65,14 +65,14 @@ class BladeDirectives
         $manifest = $this->manifest('js/tallstackui.js');
         $js = $manifest['file'];
 
-        $html = $this->url($js);
+        $html = $this->format($js);
 
         // This was created to solve problems linked to custom CSS from plugins like Tippy.js. If
         // we have a custom css, we can load it into JS, and it will build to extra CSS. As the
         // extra CSS is not load by Vite from the project that uses TallStackUI we need to deliver
         // the CSS automatically through the <tallstackui:script /> or @tallStackUiScript directive
         if ($css = ($manifest['css'][0] ?? null)) {
-            $html .= $this->url($css);
+            $html .= $this->format($css);
         }
 
         return $html;
@@ -80,7 +80,15 @@ class BladeDirectives
 
     public function style(): string
     {
-        return $this->url($this->manifest('src/resources/css/tallstackui.css', 'file'));
+        return $this->format($this->manifest('src/resources/css/tallstackui.css', 'file'));
+    }
+
+    private function format(string $file): string
+    {
+        return (match (true) { // @phpstan-ignore-line
+            str_ends_with($file, '.js') => fn () => "<script src=\"/tallstackui/script/{$file}\" defer></script>",
+            str_ends_with($file, '.css') => fn () => "<link href=\"/tallstackui/style/{$file}\" rel=\"stylesheet\" type=\"text/css\">",
+        })();
     }
 
     private function manifest(string $file, ?string $index = null): string|array
@@ -88,21 +96,5 @@ class BladeDirectives
         $content = json_decode(file_get_contents(__DIR__.'/../../../dist/.vite/manifest.json'), true);
 
         return data_get($content[$file], $index);
-    }
-
-    private function url(string $file): string
-    {
-        return (match (true) { // @phpstan-ignore-line
-            str_ends_with($file, '.js') => function () use ($file) {
-                $route = route('tallstackui.script', ['file' => $file]);
-
-                return "<script src=\"{$route}\" defer></script>";
-            },
-            str_ends_with($file, '.css') => function () use ($file) {
-                $route = route('tallstackui.style', ['file' => $file]);
-
-                return "<link href=\"{$route}\" rel=\"stylesheet\" type=\"text/css\">";
-            },
-        })();
     }
 }
