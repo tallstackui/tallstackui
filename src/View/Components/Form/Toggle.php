565ed14f539f2093b73e34d62e852ec1de18080b
hitaloramon <hitaloramon@live.com>
2024-01-15 22:14:02 -0300

    change icon to circle toggle

--- src/View/Components/Form/Toggle.php
+++ src/View/Components/Form/Toggle.php
@@ -45,33 +45,39 @@ class Toggle extends BaseComponent implements Personalization
         return Arr::dot([
             'wrapper' => 'relative flex items-center justify-end',
             'input' => [
-                'class' => 'peer absolute inset-y-0 left-0.5 z-20 my-0.5 translate-x-0 transform cursor-pointer appearance-none rounded-full border-0 bg-white shadow transition duration-200 ease-in-out checked:bg-none checked:text-white focus:outline-none focus:ring-0 focus:ring-offset-0',
+                'class' => 'peer absolute inset-y-0 left-0.5 my-0.5 translate-x-0 transform cursor-pointer appearance-none rounded-full border-0 bg-white shadow transition duration-200 ease-in-out checked:bg-none checked:text-white focus:outline-none focus:ring-0 focus:ring-offset-0',
                 'sizes' => [
-                    'xs' => 'h-2 w-2 checked:translate-x-3',
-                    'sm' => 'h-3 w-3 checked:translate-x-4',
-                    'md' => 'h-4 w-4 checked:translate-x-5',
-                    'lg' => 'h-5 w-5 checked:translate-x-5',
+                    'xs' => 'h-2 w-2 checked:translate-x-2',
+                    'sm' => 'h-3 w-3 checked:translate-x-3',
+                    'md' => 'h-4 w-4 checked:translate-x-4',
+                    'lg' => 'h-5 w-5 checked:translate-x-4',
                 ],
             ],
             'background' => [
                 'class' => 'bg-secondary-200 dark:bg-dark-800 block cursor-pointer rounded-full transition duration-100 ease-in-out group-focus:ring-2 group-focus:ring-offset-2 peer-focus:ring-2 peer-focus:ring-offset-2',
                 'sizes' => [
-                    'xs' => 'h-3 w-6',
-                    'sm' => 'h-4 w-8',
-                    'md' => 'h-5 w-10',
-                    'lg' => 'h-6 w-11',
+                    'xs' => 'h-3 w-5',
+                    'sm' => 'h-4 w-7',
+                    'md' => 'h-5 w-9',
+                    'lg' => 'h-6 w-10',
                 ],
             ],
             'icon' => [
-                'wrapper' => 'absolute m-0.5 mr-1',
+                'wrapper' => 'absolute m-0.5 tranform transition-all p-0.5',
                 'sizes' => [
                     'xs' => 'h-2 w-2',
                     'sm' => 'h-3 w-3',
                     'md' => 'h-4 w-4',
-                    'lg' => 'h-4 w-4',
+                    'lg' => 'h-5 w-5',
+                ],
+                'translate' => [
+                    'xs' => '-translate-x-2',
+                    'sm' => '-translate-x-3',
+                    'md' => '-translate-x-4',
+                    'lg' => '-translate-x-4',
                 ],
                 'color' => [
-                    'on' => 'text-white',
+                    'on' => 'text-dark-500',
                     'off' => 'text-dark-500',
                 ],
                 'thematic' => [
--- src/resources/views/components/form/toggle.blade.php
+++ src/resources/views/components/form/toggle.blade.php
@@ -11,27 +11,39 @@
 @endphp
 
 <x-dynamic-component :component="TallStackUi::component('wrapper.radio')" :$id :$property :$error :$label :$position :$alignment :$invalidate>
-    <div @class($personalize['wrapper'])>
+    <div @class($personalize['wrapper']) 
+        @if ($thematic)
+            x-data="{ checked: darkTheme }"
+        @else
+            x-data="{ checked: false }"
+        @endif>
         <input type="checkbox"
+            x-model="checked"
             @if ($id) id="{{ $id }}" @endif
-            @if ($thematic) x-bind:value="darkTheme" x-on:click="darkTheme = !darkTheme" @endif
+            @if ($thematic) x-model="checked" x-on:click="darkTheme = !darkTheme" @endif
             {{ $attributes->class([$personalize['input.class'], $personalize['input.sizes.' . $size]]) }}>
         @if ($icons !== null)
-        <x-dynamic-component :component="TallStackUi::component('icon')" :icon="$icons[0]" @class([
-                                'right-0',
-                                $personalize['icon.wrapper'], 
-                                $personalize['icon.sizes.' . $size],
-                                $personalize['icon.color.off'] => !$thematic,
-                                $personalize['icon.thematic.light'] => $thematic,
-                             ]) />
-        <x-dynamic-component :component="TallStackUi::component('icon')" :icon="$icons[1]" @class([
-                                'left-0',
-                                $personalize['icon.wrapper'], 
-                                $personalize['icon.sizes.' . $size],
-                                $personalize['icon.color.on'] => !$thematic,
-                                $personalize['icon.thematic.dark'] => $thematic,
-                             ]) />
-        @endif
+            <x-dynamic-component :component="TallStackUi::component('icon')" 
+                                 :icon="$icons[1]" 
+                                 x-show="checked"
+                                 @class([
+                                    'translate-x-0',
+                                    $personalize['icon.wrapper'], 
+                                    $personalize['icon.sizes.' . $size],
+                                    $personalize['icon.color.off'] => !$thematic,
+                                    $personalize['icon.thematic.dark'] => $thematic,
+                                 ])  />
+            <x-dynamic-component :component="TallStackUi::component('icon')" 
+                                 :icon="$icons[0]"
+                                 x-show="!checked"
+                                 @class([
+                                    $personalize['icon.translate.' . $size],
+                                    $personalize['icon.wrapper'], 
+                                    $personalize['icon.sizes.' . $size],
+                                    $personalize['icon.color.on'] => !$thematic,
+                                    $personalize['icon.thematic.light'] => $thematic,
+                                 ]) />
+    @endif
         <div @class([
             $personalize['background.class'],
             $personalize['background.sizes.' . $size],
