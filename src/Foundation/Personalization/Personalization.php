4e8df46377ded1233b5dafa58c0ba261e49edba6
AJ <60591772+devajmeireles@users.noreply.github.com>
2024-01-20 22:52:32 -0300

    removing files

--- src/Foundation/Personalization/Personalization.php
+++ src/Foundation/Personalization/Personalization.php
@@ -19,7 +19,6 @@ use TallStackUi\View\Components\Dropdown\Items as DropdownItems;
 use TallStackUi\View\Components\Errors;
 use TallStackUi\View\Components\Form\Checkbox;
 use TallStackUi\View\Components\Form\Color;
-use TallStackUi\View\Components\Form\DatePicker;
 use TallStackUi\View\Components\Form\Error;
 use TallStackUi\View\Components\Form\FileUpload;
 use TallStackUi\View\Components\Form\Hint;
@@ -136,7 +135,6 @@ class Personalization
         $class = match ($component) {
             'checkbox' => Checkbox::class,
             'color' => Color::class,
-            'datepicker' => DatePicker::class,
             'error' => Error::class,
             'fileupload' => FileUpload::class,
             'hint' => Hint::class,
