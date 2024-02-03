9a1bdb06ff2a2bf30258b2b7617204768aae98fc
AJ <60591772+devajmeireles@users.noreply.github.com>
2024-02-02 22:38:36 -0300

    wip

--- src/Foundation/Support/Components/IconGuide.php
+++ src/Foundation/Support/Components/IconGuide.php
@@ -3,6 +3,8 @@
 namespace TallStackUi\Foundation\Support\Components;
 
 //TODO: facade this???
+use Exception;
+use Illuminate\Support\Facades\View;
 use TallStackUi\View\Components\Icon;
 
 class IconGuide
@@ -35,6 +37,7 @@ class IconGuide
         ],
     ];
 
+    /** @throws Exception */
     public static function for(Icon $component): string
     {
         $config = collect(config('tallstackui.icons'));
@@ -42,20 +45,23 @@ class IconGuide
         $type = $config->get('type');
         $style = $config->get('style');
 
+        if (! in_array($type, array_keys(self::AVAILABLE))) {
+            throw new Exception("The icon type [$type] is not supported.");
+        }
+
         foreach (array_keys($component->attributes->getAttributes()) as $attribute) {
             if (in_array($attribute, self::AVAILABLE[$type])) {
                 $style = $attribute;
             }
         }
 
-        $base = str_replace('components', '', $component->blade()->name());
+        $base = str_replace('components', '', $view = $component->blade()->name());
+        $icon = sprintf('%s.%s.%s', $type, $style, $component->icon ?? $component->name);
 
-        return sprintf(
-            '%s.%s.%s.%s',
-            $base,
-            $type,
-            $style,
-            $component->icon ?? $component->name
-        );
+        if (! View::exists($view.'.'.$icon)) {
+            throw new Exception("The icon [$icon] does not exist.");
+        }
+
+        return $base.'.'.$icon;
     }
 }
