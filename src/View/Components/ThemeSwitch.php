3a0c020276cc708b4b97d00a1c0cd9988416132c
AJ <60591772+devajmeireles@users.noreply.github.com>
2024-02-06 10:44:31 -0300

    formatting

--- src/View/Components/ThemeSwitch.php
+++ src/View/Components/ThemeSwitch.php
@@ -10,10 +10,8 @@ use TallStackUi\Foundation\Personalization\Contracts\Personalization;
 #[SoftPersonalization('themeSwitch')]
 class ThemeSwitch extends BaseComponent implements Personalization
 {
-    public function __construct(
-        public ?bool $simple = false,
-    ) {
-       //
+    public function __construct(public ?bool $icons = false)
+    {
     }
 
     public function blade(): View
@@ -24,16 +22,20 @@ class ThemeSwitch extends BaseComponent implements Personalization
     public function personalization(): array
     {
         return Arr::dot([
+            'wrapper' => 'absolute inset-0 flex h-full w-full items-center justify-center transition-opacity',
             'simple' => [
-                'wrapper' => 'text-dark-500 pointer-events-none relative inline-block h-5 w-5 transition duration-200 ease-in-out dark:text-dark-200',
-                'icon' => 'h-5 w-5'
+                'wrapper' => 'text-dark-500 dark:text-dark-200 pointer-events-none relative inline-block h-5 w-5 transition duration-200 ease-in-out',
+                'icon' => 'h-5 w-5',
             ],
             'switch' => [
                 'button' => 'focus:ring-primary-500 dark:ring-offset-dark-900 relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2',
                 'wrapper' => 'text-dark-500 pointer-events-none relative inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out',
-                'icon' => 'h-4 w-4'
+                'icons' => [
+                    'size' => 'h-4 w-4',
+                    'moon' => 'text-yellow-500',
+                    'sun' => 'text-blue-500',
+                ],
             ],
-            'span' => 'absolute inset-0 flex h-full w-full items-center justify-center transition-opacity',
         ]);
     }
 }
--- src/resources/views/components/theme-switch.blade.php
+++ src/resources/views/components/theme-switch.blade.php
@@ -5,27 +5,31 @@
             role="switch"
             aria-checked="false"
             x-on:click="darkTheme = !darkTheme"
-            @if (!$simple)
-            x-bind:class="{ 'bg-primary-500': darkTheme === true, 'bg-gray-200': darkTheme === false }"
-            @endif
-            @class([$personalize['switch.button'] => !$simple])>
-         <div @class([$personalize['switch.wrapper'] => !$simple, $personalize['simple.wrapper'] => $simple])
-              @if (!$simple) 
-              x-bind:class="{ 'translate-x-5': darkTheme === true, 'translate-x-0': darkTheme === false }" 
-              @endif>
-            <span @class($personalize['span'])
+            @if (!$icons) x-bind:class="{ 'bg-primary-500': darkTheme === true, 'bg-gray-200': darkTheme === false }" @endif
+            @class([$personalize['switch.button'] => !$icons])>
+         <div @class([$personalize['switch.wrapper'] => !$icons, $personalize['simple.wrapper'] => $icons])
+              @if (!$icons) x-bind:class="{ 'translate-x-5': darkTheme === true, 'translate-x-0': darkTheme === false }"@endif>
+            <span @class($personalize['wrapper'])
                   aria-hidden="true"
                   x-bind:class="{ 'opacity-0 duration-100 ease-out': darkTheme === true, 'opacity-100 duration-200 ease-in': darkTheme === false }">
                <x-dynamic-component :component="TallStackUi::component('icon')"
                                     :icon="TallStackUi::icon('sun')"
-                                    @class([$personalize['switch.icon'] => !$simple, $personalize['simple.icon'] => $simple]) />
+                                    @class([
+                                        $personalize['switch.icons.size'],
+                                        $personalize['switch.icons.moon'] => !$icons,
+                                        $personalize['simple.icon'] => $icons
+                                    ]) />
             </span>
-            <span @class($personalize['span'])
+            <span @class($personalize['wrapper'])
                   aria-hidden="true"
                   x-bind:class="{ 'opacity-100 duration-200 ease-in': darkTheme === true, 'opacity-0 duration-100 ease-out': darkTheme === false }">
                <x-dynamic-component :component="TallStackUi::component('icon')"
                                     :icon="TallStackUi::icon('moon')"
-                                    @class([$personalize['switch.icon'] => !$simple, $personalize['simple.icon'] => $simple]) />
+                                    @class([
+                                        $personalize['switch.icons.size'],
+                                        $personalize['switch.icons.sun'] => !$icons,
+                                        $personalize['simple.icon'] => $icons
+                                    ]) />
             </span>
         </div>
     </button>
