9b1eb8ef2e0b4916458760630ea087d67c445714
Hítalo Ramon <hitaloramon@live.com>
2024-02-09 20:33:54 -0300

    fix input with icon

--- src/resources/views/components/form/input.blade.php
+++ src/resources/views/components/form/input.blade.php
@@ -21,17 +21,21 @@
             $personalize['input.class.color.base'] => !$error,
             $personalize['input.class.color.background'] => !$attributes->get('disabled') && !$attributes->get('readonly'),
             $personalize['input.class.color.disabled'] => $attributes->get('disabled') || $attributes->get('readonly'),
-            $personalize['input.paddings.left'] => $icon && ($position === null || $position === 'left'),
-            $personalize['input.paddings.right'] => $icon && $position === 'right',
             $personalize['error'] => $error
         ])>
         @if ($prefix)
-            <span @class(['ml-2 mr-1', $personalize['input.class.slot'], $personalize['error'] => $error])>{{ $prefix }}</span>
+            <span @class(['ml-2 mr-1 autofill:bg-red-200', $personalize['input.class.slot'], $personalize['error'] => $error])>{{ $prefix }}</span>
         @endif
         <input @if ($id) id="{{ $id }}" @endif
                type="{{ $attributes->get('type', 'text') }}"
                @if ($prefix || $suffix) autocomplete="off" @endif
-               {{ $attributes->class(['pr-2 pl-0' => $prefix, 'pl-2 pr-0' => $suffix, $personalize['input.class.base']]) }}>
+               {{ $attributes->class([
+                    'pr-2 pl-0' => $prefix, 
+                    'pl-2 pr-0' => $suffix, 
+                    $personalize['input.class.base'],
+                    $personalize['input.paddings.left'] => $icon && ($position === null || $position === 'left'),
+                    $personalize['input.paddings.right'] => $icon && $position === 'right'
+                ]) }}>
         @if ($suffix)
             <span @class(['ml-1 mr-2', $personalize['input.class.slot'], $personalize['error'] => $error])>{{ $suffix }}</span>
         @endif
