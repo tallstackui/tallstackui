979986adf8048161e65d1cda477645f114d6058e
Hítalo Ramon <hitaloramon@live.com>
2024-02-15 22:47:02 -0300

    wip

--- src/resources/views/components/step/step.blade.php
+++ src/resources/views/components/step/step.blade.php
@@ -18,22 +18,25 @@
     @if ($helpers)
         <div class="flex justify-between">
             <div>
-                <button type="button" x-show="selected > 1" x-on:click="selected--;" class="inline-flex items-center">
+                <button type="button" x-show="selected > 1" x-on:click="selected--;" class="inline-flex items-center text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-70">
                     <x-dynamic-component :component="TallStackUi::component('icon')"
                                          :icon="TallStackUi::icon('chevron-left')"
-                                         class="w-5 h-5" />
+                                         class="w-4 h-4 mr-1" />
                     {{ __('tallstack-ui::messages.step.previous') }}
                 </button>
             </div>
             <div>
-                <button type="button" x-show="selected < steps.length" x-on:click="selected++;" class="inline-flex items-center">
+                <button type="button" x-show="selected < steps.length" x-on:click="selected++;" class="inline-flex items-center text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-70">
                     {{ __('tallstack-ui::messages.step.next') }}
                     <x-dynamic-component :component="TallStackUi::component('icon')"
                                          :icon="TallStackUi::icon('chevron-right')"
-                                         class="w-5 h-5" />
+                                         class="w-4 h-4 ml-1" />
                 </button>
                 @if ($finish)
-                    <button type="button" x-show="selected == steps.length" x-on:click="finish()" {{ $attributes->only('x-on:finish') }}>
+                    <button type="button" 
+                            x-show="selected == steps.length" 
+                            x-on:click="finish()" {{ $attributes->only('x-on:finish') }}
+                            class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-70">
                         {{ __('tallstack-ui::messages.step.finish') }}
                     </button>
                 @endif
