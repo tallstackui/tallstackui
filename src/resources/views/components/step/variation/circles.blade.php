58d21c6ad92dc2d0a402ca0fd76c6ec6828ede85
Hítalo Ramon <hitaloramon@live.com>
2024-02-15 23:08:28 -0300

    format

--- src/resources/views/components/step/variation/circles.blade.php
+++ src/resources/views/components/step/variation/circles.blade.php
@@ -1,35 +1,40 @@
-<li class="md:shrink md:basis-0 flex-1 group flex gap-x-2 md:block cursor-pointer transition-all" x-on:click="selected = item.step">
-    <div class="min-w-8 min-h-8 flex flex-col items-center md:w-full md:inline-flex md:flex-wrap md:flex-row text-md align-middle">
-        <span class="size-8 flex justify-center items-center flex-shrink-0 font-bold rounded-full "
+<li class="group flex flex-1 cursor-pointer gap-x-2 transition-all md:block md:shrink md:basis-0"
+    x-on:click="selected = item.step">
+    <div
+         class="min-w-8 min-h-8 text-md flex flex-col items-center align-middle md:inline-flex md:w-full md:flex-row md:flex-wrap">
+        <span class="size-8 flex flex-shrink-0 items-center justify-center rounded-full font-bold"
               x-bind:class="{
                   'bg-primary-500 text-white': selected > item.step,
                   'border-2 border-primary-500 text-primary-500': selected == item.step,
-                  'border-2 border-gray-300 text-gray-500 dark:text-dark-300 dark:border-dark-500': selected < item.step,
+                  'border-2 border-gray-300 text-gray-500 dark:text-dark-300 dark:border-dark-500': selected < item
+                      .step,
               }">
             <x-dynamic-component :component="TallStackUi::component('icon')"
-                                     :icon="TallStackUi::icon('check')"
-                                     x-show="selected > item.step"
-                                     class="w-5 h-5 text-white" />
-            <span x-show="selected == item.step" 
+                                 :icon="TallStackUi::icon('check')"
+                                 x-show="selected > item.step"
+                                 class="h-5 w-5 text-white" />
+            <span x-show="selected == item.step"
                   class="h-2.5 w-2.5 rounded-full bg-primary-500 transition-all"
                   aria-hidden="true"></span>
-            <span x-show="selected < item.step" x-text="item.step"></span>
+            <span x-show="selected < item.step"
+                  x-text="item.step"></span>
         </span>
         <div x-show="item.step != steps.length"
-             class="w-0.5 h-full md:mt-0 md:w-full md:h-0.5 md:flex-1 group-last:hidden transition-all"
+             class="h-full w-0.5 transition-all group-last:hidden md:mt-0 md:h-0.5 md:w-full md:flex-1"
              x-bind:class="{
                  'bg-primary-500': selected > item.step,
                  'bg-gray-200 dark:bg-dark-500': selected <= item.step,
              }">
         </div>
     </div>
-    <div class="grow md:grow-0 md:mt-3 pb-5 transition-all">
+    <div class="grow pb-5 transition-all md:mt-3 md:grow-0">
         <span x-text="item.title"
               class="block text-base font-medium transition-all"
               x-bind:class="{
                   'text-primary-500': selected > item.step,
                   'text-gray-600 dark:text-dark-200': selected <= item.step,
               }"></span>
-        <span x-text="item.description" class="text-sm font-medium text-gray-500 dark:text-dark-400 transition-all"></span>
+        <span x-text="item.description"
+              class="text-sm font-medium text-gray-500 transition-all dark:text-dark-400"></span>
     </div>
 </li>
--- src/resources/views/components/step/variation/panels.blade.php
+++ src/resources/views/components/step/variation/panels.blade.php
@@ -1,4 +1,5 @@
-<li class="cursor-pointer relative md:flex md:flex-1" x-on:click="selected = item.step">
+<li class="relative cursor-pointer md:flex md:flex-1"
+    x-on:click="selected = item.step">
     <div class="group flex w-full items-center">
         <span class="flex items-center px-6 py-4 text-sm font-medium">
             <span class="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full"
@@ -16,17 +17,27 @@
                 <x-dynamic-component :component="TallStackUi::component('icon')"
                                      :icon="TallStackUi::icon('check')"
                                      x-show="selected > item.step"
-                                     class="w-5 h-5 text-white" />
+                                     class="h-5 w-5 text-white" />
             </span>
             <div class="flex flex-col">
-                <span class="ml-4 text-base font-bold text-primary-500 dark:text-dark-100 whitespace-nowrap" x-text="item.title"></span>
-                <span class="ml-4 text-xs font-medium text-gray-500 dark:text-dark-400 whitespace-nowrap" x-text="item.description"></span>
+                <span class="ml-4 whitespace-nowrap text-base font-bold text-primary-500 dark:text-dark-100"
+                      x-text="item.title"></span>
+                <span class="ml-4 whitespace-nowrap text-xs font-medium text-gray-500 dark:text-dark-400"
+                      x-text="item.description"></span>
             </div>
         </span>
     </div>
-    <div x-show="item.step != steps.length" class="absolute right-0 top-0 hidden h-full w-5 md:block" aria-hidden="true">
-        <svg class="h-full w-full text-dark-300 dark:text-dark-700" viewBox="0 0 22 80" fill="none" preserveAspectRatio="none">
-            <path d="M0 -2L20 40L0 82" vector-effect="non-scaling-stroke" stroke="currentcolor" stroke-linejoin="round" />
+    <div x-show="item.step != steps.length"
+         class="absolute right-0 top-0 hidden h-full w-5 md:block"
+         aria-hidden="true">
+        <svg class="h-full w-full text-dark-300 dark:text-dark-700"
+             viewBox="0 0 22 80"
+             fill="none"
+             preserveAspectRatio="none">
+            <path d="M0 -2L20 40L0 82"
+                  vector-effect="non-scaling-stroke"
+                  stroke="currentcolor"
+                  stroke-linejoin="round" />
         </svg>
     </div>
 </li>
