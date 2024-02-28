a5df653c56ef8201bef60289131bb6419f1ddc02
AJ <60591772+devajmeireles@users.noreply.github.com>
2024-02-28 00:49:43 -0300

    fixing box offset

--- src/resources/views/components/form/upload.blade.php
+++ src/resources/views/components/form/upload.blade.php
@@ -72,7 +72,7 @@
          x-transition:leave="transition ease-in duration-75"
          x-transition:leave-start="opacity-100 translate-y-0"
          x-transition:leave-end="opacity-0 -translate-y-2"
-         @if (!$hint) x-anchor.bottom-end.offset.0="$refs.wrapper" @else x-anchor.bottom-end.offset.-25="$refs.wrapper" @endif
+         x-anchor.bottom-end="$refs.anchor"
          @class($personalize['box.wrapper.first'])>
         <div @class($personalize['box.wrapper.second'])>
             @if (!$static)
--- src/resources/views/components/wrapper/input.blade.php
+++ src/resources/views/components/wrapper/input.blade.php
@@ -4,7 +4,7 @@
     @if ($label)
         <x-dynamic-component :component="TallStackUi::component('label')" :$id :$label :$error :$invalidate />
     @endif
-    <div @class($personalize['wrapper'])>
+    <div x-ref="anchor" @class($personalize['wrapper'])>
         {!! $slot !!}
     </div>
     @if ($hint && !$error)
