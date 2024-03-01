137ce09d3d5641f9363ddc20a8037bf9df9a6448
AJ <60591772+devajmeireles@users.noreply.github.com>
2024-03-01 00:07:07 -0300

    wip

--- src/resources/views/components/select/styled.blade.php
+++ src/resources/views/components/select/styled.blade.php
@@ -100,7 +100,7 @@
             @endif
         </button>
         <x-dynamic-component :component="TallStackUi::component('floating')"
-                             class="w-full"
+                             class="w-full overflow-auto"
                              x-anchor="$refs.button">
             <template x-if="searchable">
                 <div class="relative my-2 px-2">
