c3298d2ba6df672cdbfea14f16913e673a92ef7f
Joel H. Metz <joelmetz@gmail.com>
2024-03-05 07:37:53 -0300

    Add. German Language

---
+++ src/lang/de/messages.php
@@ -0,0 +1,114 @@
+<?php
+
+return [
+    'errors' => [
+        'title' => 'Es liegen :count validierungsfehler vor:',
+    ],
+
+    'select' => [
+        'default' => 'Wähle eine option',
+        'search' => 'Suchen sie hier nach etwas',
+        'empty' => 'keine ergebnisse gefunden',
+    ],
+
+    'toast' => [
+        'button' => [
+            'ok' => 'Ok',
+            'confirm' => 'Bestätigen',
+            'cancel' => 'Stornieren',
+        ],
+    ],
+
+    'dialog' => [
+        'button' => [
+            'ok' => 'Ok',
+            'confirm' => 'Bestätigen',
+            'cancel' => 'Stornieren',
+        ],
+    ],
+
+    'table' => [
+        'empty' => 'Keine ergebnisse gefunden.',
+        'quantity' => 'Menge',
+        'search' => 'Suchen sie hier nach etwas',
+    ],
+
+    'clipboard' => [
+        'button' => [
+            'copy' => 'Kopieren',
+            'copied' => 'Kopiert!',
+        ],
+    ],
+
+    'password' => [
+        'rules' => [
+            'title' => 'Erwartetes passwortformat:',
+            'formats' => [
+                'min' => 'Mindestens :min zeichen',
+                'numbers' => 'Mindestens eine nummer',
+                'symbols' => 'Mindestens ein symbol (:symbols)',
+                'mixed' => 'Groß-und kleinbuchstaben',
+            ],
+        ],
+    ],
+
+    'upload' => [
+        'placeholder' => 'Wähle eine datei',
+        'size' => 'Größe',
+        'upload' => 'Klicken sie hier, um zu senden',
+        'uploaded' => [
+            'single' => ':count datei abgeschickt',
+            'multiple' => ':count gesendete dateien',
+        ],
+        'error' => 'Etwas ist schief gelaufen. Bitte versuche es erneut.',
+        'static' => [
+            'empty' => [
+                'title' => 'Keine bilder',
+                'description' => 'Sie haben noch keine bilder.',
+            ],
+        ],
+    ],
+
+    'date' => [
+        'calendar' => [
+            'months' => [
+                'january' => 'Januar',
+                'february' => 'Februar',
+                'march' => 'Marsch',
+                'april' => 'April',
+                'may' => 'Mai',
+                'june' => 'Juni',
+                'july' => 'Juli',
+                'august' => 'August',
+                'september' => 'September',
+                'october' => 'Oktober',
+                'november' => 'November',
+                'december' => 'Dezember',
+            ],
+            'week' => [
+                'sunday' => 'Sonntag',
+                'monday' => 'Montag',
+                'tuesday' => 'Dienstag',
+                'wednesday' => 'Mittwoch',
+                'thursday' => 'Donnerstag',
+                'friday' => 'Freitag',
+                'saturday' => 'Samstag',
+            ],
+        ],
+        'helpers' => [
+            'yesterday' => 'Gestern',
+            'today' => 'Heute',
+            'tomorrow' => 'Morgen',
+        ],
+    ],
+
+    'time' => [
+        'helper' => 'Aktuelle Uhrzeit',
+    ],
+
+    'step' => [
+        'next' => 'Nachfolgend',
+        'previous' => 'Vorherige',
+        'finish' => 'Beenden',
+    ],
+];
