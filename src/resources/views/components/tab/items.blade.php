56333f43f384ef15ae95cfafd6dd9c20369b6633
AJ Meireles <alvaro.meireles@live.com>
2024-05-03 12:06:20 -0300

    fixing tab items with accents

--- src/resources/views/components/tab/items.blade.php
+++ src/resources/views/components/tab/items.blade.php
@@ -3,6 +3,6 @@
     $left = is_string($left) ? $left : ($left?->toHtml() ?? null);
 @endphp
 
-<div x-show="selected === '{{ $tab }}'" role="tabpanel" x-init="tabs.push({ tab: '{{ $tab }}', right: @js($right), left: @js($left) });">
+<div x-show="selected === @js($tab)" role="tabpanel" x-init="tabs.push({ tab: @js($tab), right: @js($right), left: @js($left) });">
     {{ $slot }}
 </div>
