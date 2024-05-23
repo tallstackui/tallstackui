250d487506b86af71413e143ee297f051df97fcb
AJ Meireles <alvaro.meireles@live.com>
2024-05-22 21:07:40 -0300

    finishing tests

--- tests/Feature/Components/Boolean/IndexTest.php
+++ tests/Feature/Components/Boolean/IndexTest.php
@@ -1,14 +1,53 @@
 <?php
 
-it('can render')
+it('can render with boolean true')
     ->expect('<x-boolean :boolean="true" />')
     ->render()
     ->toContain('<svg');
 
-it('can render different icon when true')->todo();
+it('can render with boolean false')
+    ->expect('<x-boolean :boolean="false" />')
+    ->render()
+    ->toContain('<svg');
 
-it('can render different icon when false')->todo();
+it('can resolve using different aproaches', function () {
+    expect('<x-boolean :boolean="true" />')
+        ->render()
+        ->toContain('<svg')
+        ->and('<x-boolean :boolean="false" />')
+        ->render()
+        ->toContain('<svg')
+        ->and('<x-boolean :boolean="fn () => true" />')
+        ->render()
+        ->toContain('<svg')
+        ->and('<x-boolean :boolean="function () { return true; }" />')
+        ->render()
+        ->toContain('<svg')
+        ->and('<x-boolean :boolean="1" />')
+        ->render()
+        ->toContain('<svg');
+});
 
-it('can render different color when true')->todo();
+it('can render different icons', function () {
+    $component = <<<'HTML'
+    <x-boolean :boolean="true" 
+               icon-when-true="hand-thumb-up"
+               icon-when-false="hand-thumb-down" />
+    HTML;
 
-it('can render different color when false')->todo();
+    expect($component)->render()->toContain('<svg');
+});
+
+it('can render different colors', function () {
+    $component = <<<'HTML'
+    <x-boolean :boolean="true" color-when-true="red" />
+    HTML;
+
+    expect($component)->render()->toContain('text-red-500');
+
+    $component = <<<'HTML'
+    <x-boolean :boolean="false" color-when-false="yellow" />
+    HTML;
+
+    expect($component)->render()->toContain('text-yellow-500');
+});
