a014e371548977c9cffe71da60b3c0c39eb4116b
AJ Meireles <alvaro.meireles@live.com>
2024-06-06 18:11:44 -0300

    adding type button as default in the buttons when type is not set

--- src/resources/views/components/button/button.blade.php
+++ src/resources/views/components/button/button.blade.php
@@ -3,13 +3,13 @@
     $personalize = $classes();
 @endphp
 
-<{{ $tag }} @if ($href) href="{{ $href }}" @else role="button" @endif {{ $attributes->class([
+<{{ $tag }} @if ($href) href="{{ $href }}" @else role="button" @endif {{ $attributes->except('type')->class([
         $personalize['wrapper.class'],
         $personalize['wrapper.sizes.' . $size],
         $colors['background'],
         'rounded-md' => !$square && !$round,
         'rounded-full' => !$square && $round !== null,
-    ]) }} @if ($livewire && $loading) wire:loading.attr="disabled" wire:loading.class="!cursor-wait" @endif>
+    ]) }} type="{{ $attributes->get('type', 'button') }}" @if ($livewire && $loading) wire:loading.attr="disabled" wire:loading.class="!cursor-wait" @endif>
     @if ($left)
         {!! $left !!}
     @elseif ($icon && $position === 'left')
