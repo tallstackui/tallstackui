d616b1d4094f442a9faa0fcf84e9986f09640f5b
AJ Meireles <alvaro.meireles@live.com>
2024-06-14 17:23:24 -0300

    formatting

--- src/Foundation/Console/FindComponentCommand.php
+++ src/Foundation/Console/FindComponentCommand.php
@@ -8,6 +8,7 @@ use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
 use function Laravel\Prompts\suggest;
+use function Laravel\Prompts\table;
 
 class FindComponentCommand extends Command
 {
@@ -33,7 +34,7 @@ class FindComponentCommand extends Command
             ->keys()
             ->filter(fn ($component) => ! in_array($component, self::IGNORES));
 
-        $original = suggest('Select Component', $components->values()->toArray());
+        $original = suggest('Select Component', $components->values()->toArray(), required: true);
 
         $process = new Process(['grep', '-rn', $this->prefix($original), resource_path('views')]);
 
@@ -44,9 +45,9 @@ class FindComponentCommand extends Command
 
             return self::SUCCESS;
         } catch (ProcessFailedException) {
-            $this->components->error('The component ['.$original.'] was not found in use.');
+            $this->components->error('The ['.$original.'] component is not in use.');
         } catch (Exception $exception) {
-            $this->components->error('Unexpected Error Occurred: '.$exception->getMessage());
+            $this->components->error('Unexpected Error: '.$exception->getMessage());
         }
 
         return self::FAILURE;
@@ -58,6 +59,12 @@ class FindComponentCommand extends Command
             return;
         }
 
+        $rows = [];
+
+        if (($prefix = config('tallstackui.prefix'))) {
+            $this->components->info('🔍 Searching for ['.$component.'] component with prefix ['.$prefix.']');
+        }
+
         $lines = collect(explode(PHP_EOL, $output))
             ->filter()
             // We need to ignore lines that contain </ because
@@ -67,9 +74,9 @@ class FindComponentCommand extends Command
 
         $total = $lines->count();
 
-        $this->components->info('🎉 Found '.$total.' usage occurrences of the ['.$component.'] component.');
+        $this->components->info('🎉 '.$total.' occurrences found.');
 
-        $lines->each(function (string $line, int $key) use ($total) {
+        $lines->each(function (string $line) use (&$rows) {
             preg_match('/^(.*?):(\d+):(.*)$/', $line, $matches);
 
             if (blank($line) || count($matches) !== 4) {
@@ -79,17 +86,10 @@ class FindComponentCommand extends Command
             $path = str($matches[1])->afterLast(base_path().'/')->value();
             $number = $matches[2];
 
-            $this->line("File: <fg=green>{$path}</>");
-            $this->line("Line: <fg=yellow>{$number}</>");
-
-            // While we are not at the end line we add ---
-            // otherwise we just add a new line as a whitespace
-            if (($key + 1) < $total) {
-                $this->line(str_repeat('-', 3));
-            } else {
-                $this->newLine();
-            }
+            $rows[] = [$path, $number];
         });
+
+        table(['File', 'Line'], $rows);
     }
 
     private function prefix(string $component): string
