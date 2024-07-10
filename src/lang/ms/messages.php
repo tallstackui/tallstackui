7849d9fd3257f235c51ba016c399bc7bfc3713f5
Zulkifli Mohamed <mr.putera@gmail.com>
2024-07-10 16:18:45 +0800

    Add support for Malay (Malaysia) language.

---
+++ src/lang/ms/messages.php
@@ -0,0 +1,114 @@
+<?php
+
+return [
+    'errors' => [
+        'title' => 'Terdapat :count ralat pengesahan:',
+    ],
+
+    'select' => [
+        'default' => 'Pilihan',
+        'search' => 'Carian',
+        'empty' => 'Tiada data dijumpai',
+    ],
+
+    'toast' => [
+        'button' => [
+            'ok' => 'Ok',
+            'confirm' => 'Pasti',
+            'cancel' => 'Batal',
+        ],
+    ],
+
+    'dialog' => [
+        'button' => [
+            'ok' => 'Ok',
+            'confirm' => 'Pasti',
+            'cancel' => 'Batal',
+        ],
+    ],
+
+    'table' => [
+        'empty' => 'Tiada data dijumpai',
+        'quantity' => 'Kuantiti',
+        'search' => 'Carian',
+    ],
+
+    'clipboard' => [
+        'button' => [
+            'copy' => 'Salin',
+            'copied' => 'Disalin!',
+        ],
+    ],
+
+    'password' => [
+        'rules' => [
+            'title' => 'Format Kata Laluan:',
+            'formats' => [
+                'min' => 'Mestilah mempunyai sekurang-kurangnya :min karakter',
+                'numbers' => 'Mestilah mempunyai sekurang-kurangnya satu nombor',
+                'symbols' => 'Mestilah mempunyai sekurang-kurangnya satu simbol (:symbols)',
+                'mixed' => 'Mestilah mempunyai sekurang-kurangnya satu huruf besar',
+            ],
+        ],
+    ],
+
+    'upload' => [
+        'placeholder' => 'Pilih fail',
+        'size' => 'Saiz',
+        'upload' => 'Klik untuk muat naik',
+        'uploaded' => [
+            'single' => ':count fail dihantar',
+            'multiple' => ':count fail dihantar',
+        ],
+        'error' => 'Terdapat ralat. Sila cuba lagi.',
+        'static' => [
+            'empty' => [
+                'title' => 'Tiada gambar.',
+                'description' => 'Anda tidak mempunyai gambar.',
+            ],
+        ],
+    ],
+
+    'date' => [
+        'calendar' => [
+            'months' => [
+                'january' => 'Januari',
+                'february' => 'Februari',
+                'march' => 'Mar',
+                'april' => 'April',
+                'may' => 'Mei',
+                'june' => 'Jun',
+                'july' => 'Julai',
+                'august' => 'Ogos',
+                'september' => 'September',
+                'october' => 'Oktober',
+                'november' => 'November',
+                'december' => 'Disember',
+            ],
+            'week' => [
+                'sunday' => 'Ahad',
+                'monday' => 'Isnin',
+                'tuesday' => 'Selasa',
+                'wednesday' => 'Rabu',
+                'thursday' => 'Khamis',
+                'friday' => 'Jumaat',
+                'saturday' => 'Sabtu',
+            ],
+        ],
+        'helpers' => [
+            'yesterday' => 'Semalam',
+            'today' => 'Hari Ini',
+            'tomorrow' => 'Esok',
+        ],
+    ],
+
+    'time' => [
+        'helper' => 'Masa Sekarang',
+    ],
+
+    'step' => [
+        'next' => 'Seterusnya',
+        'previous' => 'Sebelumnya',
+        'finish' => 'Selesai',
+    ],
+];
