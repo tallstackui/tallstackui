84bc394d5294f6668dd024e9f9f67bd13beee834
AJ <alvaro.meireles@live.com>
2024-07-25 11:15:58 -0300

    introducing built function and enhancements on the code

--- src/Http/Controllers/TallStackUiAssetsController.php
+++ src/Http/Controllers/TallStackUiAssetsController.php
@@ -2,27 +2,35 @@
 
 namespace TallStackUi\Http\Controllers;
 
+use Exception;
 use Livewire\Drawer\Utils;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
+use TallStackUi\Foundation\Support\Blade\BladeDirectives;
 
 class TallStackUiAssetsController
 {
+    /** @throws Exception */
     public function script(?string $file = null): Response|BinaryFileResponse
     {
-        if (! file_exists($file)) {
-            abort(404);
+        if (! file_exists($path = __DIR__.'/../../../dist/'.$file)) {
+            $files = BladeDirectives::built('js');
+
+            abort(new Response('File not found: '.$file.'. Available files: '.implode(', ', $files), 404));
         }
 
-        return Utils::pretendResponseIsFile(__DIR__.'/../../../dist/'.$file, 'text/javascript');
+        return Utils::pretendResponseIsFile($path, 'text/javascript');
     }
 
+    /** @throws Exception */
     public function style(?string $file = null): Response|BinaryFileResponse
     {
-        if (! file_exists($file)) {
-            abort(404);
+        if (! file_exists($path = __DIR__.'/../../../dist/'.$file)) {
+            $files = BladeDirectives::built('js');
+
+            abort(new Response('File not found: '.$file.'. Available files: '.implode(', ', $files), 404));
         }
 
-        return Utils::pretendResponseIsFile(__DIR__.'/../../../dist/'.$file, 'text/css');
+        return Utils::pretendResponseIsFile($path, 'text/css');
     }
 }
