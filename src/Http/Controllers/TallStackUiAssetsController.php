c4e12d69e6f8076dc9ff05f78d654ce08d8f7976
lguichard <lguichard@business-first.io>
2024-07-25 10:08:04 +0200

    [1.x] Fixing file not found

--- src/Http/Controllers/TallStackUiAssetsController.php
+++ src/Http/Controllers/TallStackUiAssetsController.php
@@ -10,11 +10,19 @@ class TallStackUiAssetsController
 {
     public function script(?string $file = null): Response|BinaryFileResponse
     {
+        if (! file_exists($file)) {
+            abort(404);
+        }
+
         return Utils::pretendResponseIsFile(__DIR__.'/../../../dist/'.$file, 'text/javascript');
     }
 
     public function style(?string $file = null): Response|BinaryFileResponse
     {
+        if (! file_exists($file)) {
+            abort(404);
+        }
+
         return Utils::pretendResponseIsFile(__DIR__.'/../../../dist/'.$file, 'text/css');
     }
 }
