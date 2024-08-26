b4f18eb7da0bb77e9a68b4c622180bc5ff32ae25
AJ Meireles <alvaro.meireles@live.com>
2024-08-26 13:23:40 -0300

    wip

--- src/Foundation/Support/Blade/Directives.php
+++ src/Foundation/Support/Blade/Directives.php
@@ -79,8 +79,8 @@ class Directives
         // we have a custom css, we can load it into JS, and it will build to extra CSS. As the
         // extra CSS is not load by Vite from the project that uses TallStackUI we need to deliver
         // the CSS automatically through the <tallstackui:script /> or @tallStackUiScript directive
-        if ($css = ($manifest['css'][0] ?? null)) {
-            $html .= $this->format($css, 'tippy');
+        if (($manifest['css'][0] ?? null) !== null) {
+            $html .= $this->format($this->manifest('node_modules/tippy.js/dist/tippy.css', 'file'), 'tippy');
         }
 
         return $html;
