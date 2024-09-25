013737af417d354443eb949c0e6b9f34bd1723b3
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-09-25 12:19:15 -0300

    Removing duplicate classes
    'cursor-default' applies the same CSS properties as 'cursor-pointer'.

--- src/resources/views/components/table/paginators.blade.php
+++ src/resources/views/components/table/paginators.blade.php
@@ -19,22 +19,22 @@ $scrollIntoViewJsSnippet = ($scrollTo !== false)
             <div @class(['flex flex-1 gap-x-2 mb-4', 'justify-end' => $simplePagination, 'justify-between sm:hidden' => !$simplePagination])>
                 <span>
                     @if ($paginator->onFirstPage())
-                        <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-400 dark:text-dark-500 bg-gray-100 dark:bg-dark-700 border border-gray-200 dark:border-transparent cursor-default leading-5 rounded-md select-none cursor-pointer">
+                        <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-400 bg-gray-100 border border-gray-200 rounded-md cursor-default cursor-pointer select-none dark:text-dark-500 dark:bg-dark-700 dark:border-transparent">
                             {!! trans('pagination.previous') !!}
                         </span>
                     @else
-                        <button type="button" wire:click="previousPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" wire:loading.attr="disabled" dusk="previousPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.before" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 dark:text-dark-300 bg-white dark:bg-dark-600 border border-gray-300 dark:border-transparent leading-5 rounded-md select-none cursor-pointer">
+                        <button type="button" wire:click="previousPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" wire:loading.attr="disabled" dusk="previousPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.before" class="relative inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-500 bg-white border border-gray-300 rounded-md cursor-pointer select-none dark:text-dark-300 dark:bg-dark-600 dark:border-transparent">
                             {!! trans('pagination.previous') !!}
                         </button>
                     @endif
                 </span>
                 <span>
                     @if ($paginator->hasMorePages())
-                        <button type="button" wire:click="nextPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" wire:loading.attr="disabled" dusk="nextPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.before" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 dark:text-dark-300 bg-white dark:bg-dark-600 border border-gray-300 dark:border-transparent leading-5 rounded-md select-none cursor-pointer">
+                        <button type="button" wire:click="nextPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" wire:loading.attr="disabled" dusk="nextPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.before" class="relative inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-500 bg-white border border-gray-300 rounded-md cursor-pointer select-none dark:text-dark-300 dark:bg-dark-600 dark:border-transparent">
                             {!! trans('pagination.next') !!}
                         </button>
                     @else
-                        <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-400 dark:text-dark-500 bg-gray-100 dark:bg-dark-700 border border-gray-200 dark:border-transparent cursor-default leading-5 rounded-md select-none cursor-pointer">
+                        <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-400 bg-gray-100 border border-gray-200 rounded-md cursor-default cursor-pointer select-none dark:text-dark-500 dark:bg-dark-700 dark:border-transparent">
                             {!! trans('pagination.next') !!}
                         </span>
                     @endif
@@ -44,7 +44,7 @@ $scrollIntoViewJsSnippet = ($scrollTo !== false)
             <!-- Desktop Buttons -->
             <div class="hidden sm:flex sm:items-center sm:justify-between">
                 <div class="mr-4">
-                    <p class="text-sm text-gray-700 dark:text-dark-300 leading-5">
+                    <p class="text-sm leading-5 text-gray-700 dark:text-dark-300">
                         <span>{!! trans('Showing') !!}</span>
                         <span class="font-medium">{{ $paginator->firstItem() }}</span>
                         <span>{!! trans('to') !!}</span>
@@ -60,14 +60,14 @@ $scrollIntoViewJsSnippet = ($scrollTo !== false)
                         <span>
                             @if ($paginator->onFirstPage())
                                 <span aria-disabled="true" aria-label="{{ trans('pagination.previous') }}">
-                                    <span class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-300 dark:text-dark-500 bg-gray-100 dark:bg-dark-700 border border-gray-300 dark:border-transparent cursor-default rounded-l-md leading-5 focus:outline-none" aria-hidden="true">
+                                    <span class="relative inline-flex items-center px-2 py-2 text-sm font-medium leading-5 text-gray-300 bg-gray-100 border border-gray-300 cursor-default dark:text-dark-500 dark:bg-dark-700 dark:border-transparent rounded-l-md focus:outline-none" aria-hidden="true">
                                         <x-dynamic-component :component="TallStackUi::component('icon')"
                                                              :icon="TallStackUi::icon('chevron-left')"
                                                              class="w-5 h-5" />
                                     </span>
                                 </span>
                             @else
-                                <button type="button" wire:click="previousPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" dusk="previousPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.after" rel="prev" class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 dark:text-dark-300 bg-white dark:bg-dark-600 border border-gray-300 dark:border-transparent rounded-l-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" aria-label="{{ trans('pagination.previous') }}">
+                                <button type="button" wire:click="previousPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" dusk="previousPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.after" rel="prev" class="relative inline-flex items-center px-2 py-2 text-sm font-medium leading-5 text-gray-500 transition duration-150 ease-in-out bg-white border border-gray-300 dark:text-dark-300 dark:bg-dark-600 dark:border-transparent rounded-l-md hover:text-gray-400 focus:z-10 focus:outline-none focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500" aria-label="{{ trans('pagination.previous') }}">
                                     <x-dynamic-component :component="TallStackUi::component('icon')"
                                                          :icon="TallStackUi::icon('chevron-left')"
                                                          class="w-5 h-5" />
@@ -79,7 +79,7 @@ $scrollIntoViewJsSnippet = ($scrollTo !== false)
                             <!-- "Three Dots" Separator -->
                             @if (is_string($element))
                                 <span aria-disabled="true">
-                                    <span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 dark:text-dark-300 bg-white dark:bg-dark-600 border border-gray-300 dark:border-transparent cursor-default leading-5 select-none">{{ $element }}</span>
+                                    <span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium leading-5 text-gray-700 bg-white border border-gray-300 cursor-default select-none dark:text-dark-300 dark:bg-dark-600 dark:border-transparent">{{ $element }}</span>
                                 </span>
                             @endif
                             <!-- Array Of Links -->
@@ -88,12 +88,12 @@ $scrollIntoViewJsSnippet = ($scrollTo !== false)
                                     <span wire:key="paginator-{{ $paginator->getPageName() }}-page{{ $page }}">
                                         @if ($page == $paginator->currentPage())
                                             <span aria-current="page">
-                                                <span class="relative z-10 inline-flex items-center px-4 py-2 -ml-px text-sm font-bold text-primary-700 dark:text-dark-300 bg-primary-100 dark:bg-dark-700 border border-primary-300 dark:border-transparent cursor-default select-none">
+                                                <span class="relative z-10 inline-flex items-center px-4 py-2 -ml-px text-sm font-bold border cursor-default select-none text-primary-700 dark:text-dark-300 bg-primary-100 dark:bg-dark-700 border-primary-300 dark:border-transparent">
                                                     {{ $page }}
                                                 </span>
                                             </span>
                                         @else
-                                            <button type="button" wire:click="gotoPage({{ $page }}, '{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-600 dark:text-dark-400 bg-white dark:bg-dark-600 border border-gray-300 dark:border-transparent leading-5 hover:text-gray-500 focus:z-10 focus:outline-none focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150" aria-label="{{ trans('Go to page :page', ['page' => $page]) }}">
+                                            <button type="button" wire:click="gotoPage({{ $page }}, '{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium leading-5 text-gray-600 transition duration-150 ease-in-out bg-white border border-gray-300 dark:text-dark-400 dark:bg-dark-600 dark:border-transparent hover:text-gray-500 focus:z-10 focus:outline-none focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700" aria-label="{{ trans('Go to page :page', ['page' => $page]) }}">
                                                 {{ $page }}
                                             </button>
                                         @endif
@@ -104,14 +104,14 @@ $scrollIntoViewJsSnippet = ($scrollTo !== false)
                         <span>
                             <!-- Next Page Link -->
                             @if ($paginator->hasMorePages())
-                                <button type="button" wire:click="nextPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" dusk="nextPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.after" rel="next" class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-500 dark:text-dark-300 bg-white dark:bg-dark-600 border border-gray-300 dark:border-transparent rounded-r-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" aria-label="{{ trans('pagination.next') }}">
+                                <button type="button" wire:click="nextPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" dusk="nextPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.after" rel="next" class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium leading-5 text-gray-500 transition duration-150 ease-in-out bg-white border border-gray-300 dark:text-dark-300 dark:bg-dark-600 dark:border-transparent rounded-r-md hover:text-gray-400 focus:z-10 focus:outline-none focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500" aria-label="{{ trans('pagination.next') }}">
                                     <x-dynamic-component :component="TallStackUi::component('icon')"
                                                          :icon="TallStackUi::icon('chevron-right')"
                                                          class="w-5 h-5" />
                                 </button>
                             @else
                                 <span aria-disabled="true" aria-label="{{ trans('pagination.next') }}">
-                                    <span class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-300 dark:text-dark-500 bg-gray-100 dark:bg-dark-700 border border-gray-300 dark:border-transparent cursor-default rounded-r-md leading-5" aria-hidden="true">
+                                    <span class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium leading-5 text-gray-300 bg-gray-100 border border-gray-300 cursor-default dark:text-dark-500 dark:bg-dark-700 dark:border-transparent rounded-r-md" aria-hidden="true">
                                         <x-dynamic-component :component="TallStackUi::component('icon')"
                                                              :icon="TallStackUi::icon('chevron-right')"
                                                              class="w-5 h-5" />
