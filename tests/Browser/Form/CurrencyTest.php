8045004eb9b3c1cc43592fa91fc501a0877881c8
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-10-04 19:16:32 -0300

    more test, more consistency

--- tests/Browser/Form/CurrencyTest.php
+++ tests/Browser/Form/CurrencyTest.php
@@ -54,5 +54,32 @@ class CurrencyTest extends BrowserTestCase
             ->type('@currency_input', '1000')
             ->pause(500)
             ->assertSeeIn('@money', '10,00');
-    }   
+    }
+
+    /** @test */
+    public function does_not_allow_letters(): void
+    {
+        Livewire::visit(new class extends Component
+        {
+            public ?string $money = '';
+
+            public function render(): string
+            {
+                return <<<'HTML'
+                <div>
+                    <p dusk="money">{{ $money }}</p>
+                
+                    <x-currency dusk="currency_input" wire:model.live="money" clearable />
+                </div>
+                HTML;
+            }
+        })
+            ->waitForLivewireToLoad()
+            ->type('@currency_input', '1000')
+            ->pause(500)
+            ->assertSeeIn('@money', '10.00')
+            ->type('@currency_input', '1000a')
+            ->pause(500)
+            ->assertSeeIn('@money', '10.00');
+    }
 }
