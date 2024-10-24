67746f3b1f4e430b9301e3ccf5ad7b9555f13da4
AJ Meireles <alvaro.meireles@live.com>
2024-10-23 23:53:16 -0300

    enhancements

--- src/View/Components/Select/Traits/Setup.php
+++ src/View/Components/Select/Traits/Setup.php
@@ -12,7 +12,6 @@ trait Setup
             ? $this->options->values()->toArray()
             : array_values($this->options);
 
-        // TODO: create a browser test for it.
         $this->select ??= $this->options !== [] ? 'label:label|value:value' : $this->select;
 
         if (! $this->select || ($this->options !== [] && ! is_array($this->options[0]))) {
@@ -20,16 +19,26 @@ trait Setup
         }
 
         $select = explode('|', $this->select);
-        $label = explode(':', $select[0])[1];
-        $value = explode(':', $select[1])[1];
 
-        $this->options = collect($this->options)->map(fn (array $item): array => [
-            $label => $item[$label],
-            $value => $item[$value],
-            'image' => current(array_intersect_key($item, array_flip(['image', 'img', 'img_src']))) ?: null,
-            'disabled' => $item['disabled'] ?? false,
-            'description' => current(array_intersect_key($item, array_flip(['description', 'note']))) ?: null,
-        ])->toArray();
+        [$label, $value] = array_map(fn ($item) => explode(':', $item)[1], $select);
+
+        $this->options = collect($this->options)->map(function (array $item) use ($label, $value): array {
+            if (! isset($item[$label])) {
+                throw new InvalidArgumentException("The select.styled key [$label] is missing in the options array.");
+            }
+
+            if (! isset($item[$value])) {
+                throw new InvalidArgumentException("The select.styled [$value] is missing in the options array.");
+            }
+
+            return [
+                $label => $item[$label],
+                $value => $item[$value],
+                'image' => current(array_intersect_key($item, array_flip(['image', 'img', 'img_src']))) ?: null,
+                'disabled' => $item['disabled'] ?? false,
+                'description' => current(array_intersect_key($item, array_flip(['description', 'note']))) ?: null,
+            ];
+        })->toArray();
 
         $this->selectable = ['label' => $label, 'value' => $value];
     }
