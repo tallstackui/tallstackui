27674af78f80cc370c88c674742af2c32ab75814
AJ Meireles <alvaro.meireles@live.com>
2024-10-23 23:36:54 -0300

    removing concept of transitions through method and set the time to 100

--- src/resources/views/components/card.blade.php
+++ src/resources/views/components/card.blade.php
@@ -42,10 +42,14 @@
                 @endif
             </div>
         @endif
-        <div {{ $attributes->class($personalize['body']) }} x-show="!minimize"
-                @if ($transition)
-                    {!! $transitions() !!}
-                @endif>
+        <div {{ $attributes->class($personalize['body']) }}
+             x-show="!minimize"
+             x-transition:enter="transition ease-out duration-100"
+             x-transition:enter-start="opacity-0 -translate-y-10"
+             x-transition:enter-end="opacity-100 translate-y-0"
+             x-transition:leave="transition ease-in duration-100"
+             x-transition:leave-start="opacity-100 translate-y-0"
+             x-transition:leave-end="opacity-0 -translate-y-10">
             {{ $slot }}
         </div>
         @if ($footer)
