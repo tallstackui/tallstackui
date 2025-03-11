89fc2adf6396a8891f524921ae0fb5bded500401
AJ Meireles <alvaro.meireles@live.com>
2025-03-10 23:55:32 -0300

    fixing rector issue

--- src/Foundation/Support/Runtime/Components/PasswordRuntime.php
+++ src/Foundation/Support/Runtime/Components/PasswordRuntime.php
@@ -17,10 +17,10 @@ class PasswordRuntime extends AbstractRuntime
                 'x-circle' => TallStackUi::icon('x-circle'),
                 'check-circle' => TallStackUi::icon('check-circle'),
             ],
-            'password' => ! $this->data('simple') ? [
+            'password' => $this->data('simple') ? [] : [
                 'x-on:click' => 'rules = true',
                 'x-model.debounce' => 'input',
-            ] : [],
+            ],
         ];
     }
 }
