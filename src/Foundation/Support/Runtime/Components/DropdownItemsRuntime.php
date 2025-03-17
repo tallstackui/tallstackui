74dbfb7d1e47710da6d1de9fe897d4f2251842c2
mchev <martin.chevignard@gmail.com>
2025-03-17 08:19:53 +0100

    pintable

--- src/Foundation/Support/Runtime/Components/DropdownItemsRuntime.php
+++ src/Foundation/Support/Runtime/Components/DropdownItemsRuntime.php
@@ -8,10 +8,6 @@ class DropdownItemsRuntime extends AbstractRuntime
 {
     public function runtime(): array
     {
-        return ['tag' => filled($this->data('href')) 
-            ? 'a' 
-            : ($this->data('text') 
-                ? 'button' 
-                : 'div')];
+        return ['tag' => filled($this->data('href')) ? 'a' : ($this->data('text') ? 'button' : 'div')];
     }
 }
