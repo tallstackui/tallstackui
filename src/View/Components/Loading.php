2b250b80fc8ecc813275b23f7a25fc9ec616fd65
AJ Meireles <alvaro.meireles@live.com>
2025-03-25 16:33:35 -0300

    wip

--- src/View/Components/Loading.php
+++ src/View/Components/Loading.php
@@ -38,7 +38,7 @@ class Loading extends TallStackUiComponent implements Personalization
                 'first' => 'fixed inset-0 bg-gray-300 dark:bg-dark-600',
                 'second' => 'flex h-full items-center justify-center',
             ],
-            'opacity' => 'opacity-80 dark:opacity-70',
+            'opacity' => 'opacity-75 dark:opacity-70',
             'blur' => 'backdrop-blur-sm',
             'spinner' => 'h-12 w-12 animate-spin text-primary-700 dark:text-white',
             'text' => 'inline-flex items-center text-lg font-semibold text-primary-500',
