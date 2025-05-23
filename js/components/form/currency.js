a40bb1a595b3c0993827acb28f9dcac30ad82d08
AJ Meireles <alvaro.meireles@live.com>
2025-05-23 00:59:04 -0300

    wip

--- js/components/form/currency.js
+++ js/components/form/currency.js
@@ -1,24 +1,45 @@
-export default (locale) => ({
-  locale: locale,
-  init() {
-    this.$refs.input.addEventListener('input', () => {
-      this.format();
-    });
-  },
-  /**
-   * Format the input value based on locale
-   *
-   * @returns {void}
-   */
-  format() {
-    let value = this.$refs.input.value.replace(/[^\d]/g, '');
-    if (value === '') return;
+export default (model, property, locale) => {
+  return ({
+    model: model,
+    input: '',
+    locale: locale,
+    init() {
+      if (this.model) {
+        this.input = this.model;
 
-    value = parseFloat(value) / 100; // Convert to currency
+        this.$nextTick(() => this.format());
+      }
 
-    this.$refs.input.value = new Intl.NumberFormat(this.locale, {
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 4
-    }).format(value);
-  },
-});
+      this.$watch('input', (value) => {
+        this.format(value);
+
+        this.model = this.input;
+      });
+    },
+    /**
+     * Format the input value based on locale
+     *
+     * @returns {void}
+     */
+    format(value = null) {
+      let current = '';
+
+      if (value) {
+        current = value.replace(/[^\d]/g, '');
+      } else {
+        // current = this.$refs[property].value.replace(/[^\d]/g, '');
+        current = this.input;
+      }
+
+      if (current === '') return;
+
+      current = parseFloat(current) / 100; // Convert to currency
+
+      this.input = new Intl.NumberFormat(this.locale, {
+        // TODO it should be options (like: decimal, position, etc)
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 4
+      }).format(current);
+    },
+  });
+};
---
+++ src/Foundation/Support/Runtime/Components/CurrencyRuntime.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace TallStackUi\Foundation\Support\Runtime\Components;
+
+use Exception;
+use TallStackUi\Foundation\Support\Runtime\AbstractRuntime;
+
+class CurrencyRuntime extends AbstractRuntime
+{
+    /** @throws Exception */
+    public function runtime(): array
+    {
+        $bind = $this->bind();
+
+        return [
+            'entangle' => $bind->get('entangle'),
+            'property' => $property = $bind->get('property'),
+            'error' => $bind->get('error'),
+            'id' => $bind->get('id'),
+            'ref' => $property ?? uniqid(),
+        ];
+    }
+}
