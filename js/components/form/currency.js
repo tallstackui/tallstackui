8f7ea94d3b0f3569473ae9662e2bd620b93075cb
AJ Meireles <alvaro.meireles@live.com>
2025-05-23 16:35:25 -0300

    formatting

--- js/components/form/currency.js
+++ js/components/form/currency.js
@@ -4,59 +4,57 @@ export default (
     precision,
     clearable,
     locale
-) => {
-    return ({
-        model: model,
-        input: '',
-        decimals: decimals,
-        precision: precision,
-        clearable: clearable,
-        locale: locale,
-        init() {
-            if (this.model) {
-                this.input = this.model;
+) => ({
+    model: model,
+    input: '',
+    decimals: decimals,
+    precision: precision,
+    clearable: clearable,
+    locale: locale,
+    init() {
+        if (this.model) {
+            this.input = this.model;
 
-                this.$nextTick(() => this.format());
-            }
-
-            this.$watch('input', (value) => this.format(value));
-        },
-        /**
-         * Format the input value.
-         *
-         * @returns {void}
-         */
-        format(value = null) {
-            let current = value ? value.replace(/\D/g, '') : this.input;
-
-            if (current === '') {
-                return;
-            }
-
-            current = parseFloat(current) / 100;
-
-            this.input = new Intl.NumberFormat(this.locale, {
-                minimumFractionDigits: this.decimals,
-                maximumFractionDigits: this.precision
-            }).format(current);
-        },
-        /**
-         * Sync the input value with the model.
-         *
-         * @returns {void}
-         */
-        sync () {
-            this.$nextTick(() => this.model = this.input.replace(/\D/g, ''));
-        },
-        /**
-         * Clear the input.
-         *
-         * @returns {void}
-         */
-        clear() {
-            this.input = '';
-
-            this.model = null;
+            this.$nextTick(() => this.format());
         }
-    });
-};
+
+        this.$watch('input', (value) => this.format(value));
+    },
+    /**
+     * Format the input value.
+     *
+     * @returns {void}
+     */
+    format(value = null) {
+        let current = value ? value.replace(/\D/g, '') : this.input;
+
+        if (current === '') {
+            return;
+        }
+
+        current = parseFloat(current) / 100;
+
+        this.input = new Intl.NumberFormat(this.locale, {
+            minimumFractionDigits: this.decimals,
+            maximumFractionDigits: this.precision,
+        }).format(current);
+    },
+    /**
+     * Sync the input value with the model.
+     *
+     * @returns {void}
+     */
+    sync() {
+        this.$nextTick(() => (this.model = this.input.replace(/\D/g, '')));
+    },
+    /**
+     * Clear the input.
+     *
+     * @returns {void}
+     */
+    clear() {
+        this.input = '';
+
+        this.model = null;
+    },
+});
