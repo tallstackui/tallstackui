9b1ba35a839e98c4ca8d1455e5a0ff9a1d679b3b
delacroix1983 <104434446+delacroix1983@users.noreply.github.com>
2025-09-29 20:17:44 +0200

    Update Card.php
    Add body-table CSS

--- src/View/Components/Card.php
+++ src/View/Components/Card.php
@@ -24,6 +24,7 @@ class Card extends TallStackUiComponent implements Personalization
         public ?bool $close = null,
         public ?string $image = null,
         public ?string $position = 'top',
+        public ?bool $table = false,
         #[SkipDebug]
         public ?bool $initializeMinimized = false,
         #[SkipDebug]
@@ -66,6 +67,7 @@ class Card extends TallStackUiComponent implements Personalization
                 ],
             ],
             'body' => 'text-secondary-700 dark:text-dark-300 grow rounded-b-xl px-4 py-5',
+            'body-table' => 'text-secondary-700 dark:text-dark-300 grow rounded-b-xl',
             'footer' => [
                 'wrapper' => 'text-secondary-700 dark:text-dark-300 dark:border-t-dark-600 rounded-lg rounded-t-none border-t border-t-secondary-200 p-4',
                 'text' => 'flex items-center justify-end gap-2',
