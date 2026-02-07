5bd90715e42bbf9612fba8212cc012fa932ee3e6
AJ Meireles <alvaro.meireles@live.com>
2026-02-07 12:11:38 -0300

    feat: render id attribute on step items DOM element when provided

--- src/resources/views/components/step/items.blade.php
+++ src/resources/views/components/step/items.blade.php
@@ -1,5 +1,6 @@
 <div role="tabpanel"
      x-show="parseInt(selected) === {{ $step }}"
-     x-init="steps.push({ step: @js($step), title: @js($title), description: @js($description), completed: @js($completed), id: @js($id) })">
+     x-init="steps.push({ step: @js($step), title: @js($title), description: @js($description), completed: @js($completed), id: @js($id) })"
+     @if ($id) id="{{ $id }}" @endif>
     {{ $slot }}
 </div>
