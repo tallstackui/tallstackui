7eeb1af66d58c7073012a7a8f15e517139f8e183
AJ Meireles <alvaro.meireles@live.com>
2026-02-07 11:43:24 -0300

    feat: add scrollable property to modal component

--- src/Components/Modal/Component.php
+++ src/Components/Modal/Component.php
@@ -43,8 +43,8 @@ class Component extends TallStackUiComponent implements Customization
             'wrapper' => [
                 'first' => 'fixed inset-0 bg-gray-400/75 transform transition-opacity',
                 'second' => 'fixed inset-0 z-50 w-screen overflow-y-auto',
-                'third' => 'mx-auto flex min-h-full w-full transform justify-center p-4',
-                'fourth' => 'dark:bg-dark-700 relative flex w-full transform flex-col rounded-xl bg-white text-left shadow-xl transition-all',
+                'third' => 'mx-auto flex min-h-full w-full transform justify-center sm:p-4',
+                'fourth' => 'dark:bg-dark-700 relative flex w-full transform flex-col rounded-t-xl sm:rounded-xl bg-white text-left shadow-xl transition-all',
                 'scrollable' => 'max-h-[80vh] flex flex-col',
             ],
             'positions' => [
