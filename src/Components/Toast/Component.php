d083e335741ccfa6589961150db7078a2f8ef783
AJ Meireles <alvaro.meireles@live.com>
2026-02-09 14:28:40 -0300

    wip

--- src/Components/Toast/Component.php
+++ src/Components/Toast/Component.php
@@ -15,8 +15,6 @@ use TallStackUi\TallStackUiComponent;
 #[ColorsThroughOf(ToastColors::class)]
 class Component extends TallStackUiComponent implements Customization
 {
-    public function __construct(public ?string $simplevisual = null) {}
-
     public function blade(): View
     {
         return view('ts-ui::components.toast.main');
