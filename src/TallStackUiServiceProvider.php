604c2402d4d30ae6fb8bf4458c2face41234c8af
AJ Meireles <alvaro.meireles@live.com>
2026-05-31 22:58:50 -0300

    feat(table): add table-shadowless predefined scope

--- src/TallStackUiServiceProvider.php
+++ src/TallStackUiServiceProvider.php
@@ -161,6 +161,12 @@ class TallStackUiServiceProvider extends ServiceProvider
             ->tab()
             ->block('base.wrapper')
             ->remove('shadow-md')
-            ->append('border border-gray-300');
+            ->append('border border-gray-300')
+            ->and()
+            // table
+            ->scope('table-shadowless')
+            ->table()
+            ->block('wrapper')
+            ->remove('shadow');
     }
 }
