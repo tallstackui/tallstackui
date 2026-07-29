813d80e415e41611c7f18bee84c00e7b8d3ab73a
Gerardo <gejobj@gmail.com>
2026-07-29 18:12:08 +0200

    Add 'top-center' and 'bottom-center' to toast positions

--- src/Interactions/Toast.php
+++ src/Interactions/Toast.php
@@ -89,8 +89,8 @@ class Toast extends AbstractInteraction
      */
     public function position(string $position): self
     {
-        if (! in_array($position, ['top-right', 'top-left', 'bottom-right', 'bottom-left'])) {
-            __ts_validation_exception(Component::class, "Invalid position: {$position}. Allowed: top-right, top-left, bottom-right, bottom-left.");
+        if (! in_array($position, ['top-right', 'top-left', 'top-center', 'bottom-right', 'bottom-left', 'bottom-center'])) {
+            __ts_validation_exception(Component::class, "Invalid position: {$position}. Allowed: top-right, top-left, top-center, bottom-right, bottom-left, bottom-center.");
         }
 
         $this->position = $position;
