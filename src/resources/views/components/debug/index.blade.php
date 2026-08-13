8be6c0ea16d004389422b6fcbc277129181a9f42
AJ Meireles <alvaro.meireles@live.com>
2026-08-12 21:33:59 -0300

    feat(debug): open the overlay tooltip instantly
    The property dump now uses the flash tooltip delay.

--- src/resources/views/components/debug/index.blade.php
+++ src/resources/views/components/debug/index.blade.php
@@ -5,7 +5,9 @@
     @if ($code)
         <x-dynamic-component component="ts-ui::icon.generic.code-bracket-square"
                              data-position="auto"
+                             x-data
                              x-tooltip="{!! $code !!}"
+                             data-tooltip-delay="flash"
                              class="w-4 h-4 text-red-500 dark:text-white" />
     @endif
 </div>
